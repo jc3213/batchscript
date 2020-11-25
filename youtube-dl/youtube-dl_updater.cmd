9df956108596a405b79475e8b55057a0897d928a
jc3213 <jc3213@users.noreply.github.com>
2020-11-25 20:25:33 +0800

    update logic

--- youtube-dl/youtube-dl_updater.cmd
+++ youtube-dl/youtube-dl_updater.cmd
@@ -8,26 +8,46 @@ FOR /F "usebackq tokens=1-2 delims==" %%I IN ("youtube-dl.conf") DO (
     IF "%%I"=="proxy" SET proxy=%%J
 )
 :Wizard
-CALL :Proxy
+CALL :Bypass
 CALL :Space
 CALL :Updater
 PAUSE
 GOTO :Wizard
 ::
-:Proxy
-CALL :Warn Proxy
-SET /P option_p=Use Proxy Server: 
-IF "%option_p%"=="1" GOTO :Server
-IF "%option_p%"=="0" EXIT /B
-GOTO :Proxy
+:Bypass
+IF DEFINED proxy (
+    CALL :Warn Bypass
+    SET /P pass=Use Proxy Server:
+) ELSE (
+    GOTO :Server
+)
+IF "%pass%"=="2" GOTO :Server
+IF "%pass%"=="1" GOTO :Proxy
+IF "%pass%"=="0" EXIT /B
+GOTO :Bypass
 :Server
-CALL :Warn Server
-IF DEFINED proxy (ECHO Proxy Server: %proxy%) ELSE (SET /P proxy=Proxy Server: )
-IF DEFINED proxy SET server=--proxy "!proxy!"
+SET proxy=
+SET /P proxy=Proxy Server: 
+IF DEFINED proxy GOTO :Proxy
+GOTO :Server
+:Proxy
+SET server=--proxy "!proxy!"
 EXIT /B
 ::
 :Updater
-bin\youtube-dl.exe %server% --update --verbose
+SET attempt=0
+IF NOT DEFINED retry SET retry=5
+:Process
+IF "%retry%"=="%attempt%" (
+ECHO "%retry%"=="%attempt%"
+    PAUSE
+    GOTO :Wizard
+)
+bin\youtube-dl.exe %server% --update --verbose || (
+    TIMEOUT /T 5
+    SET /A attempt=%attempt%+1
+    GOTO :Process
+)
 EXIT /B
 :Space
 ECHO.
@@ -39,8 +59,9 @@ ECHO ===========================================================================
 CALL :Warn_%1
 ECHO ========================================================================================
 EXIT /B
-:Warn_Proxy
-ECHO 1. Yes
+:Warn_Bypass
+ECHO 2. Other
+ECHO 1. Yes ^(%proxy%^)
 ECHO 0. No
 EXIT /B
 :Warn_Server
