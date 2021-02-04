a3db0c5032f81ab2f8df5e4f8ae0c9756f45b795
jc3213 <jc3213@users.noreply.github.com>
2021-02-04 20:18:00 +0800

    ability to change settings and update under URL

--- youtube-dl/youtube_dl.cmd
+++ youtube-dl/youtube_dl.cmd
@@ -11,6 +11,7 @@ FOR /F "usebackq tokens=1-2 delims==" %%I IN ("youtube-dl.conf") DO (
     IF "%%I"=="history" set history=%%J
     IF "%%I"=="retry" set retry=%%J
 )
+IF NOT DEFINED retry SET retry=5
 :Wizard
 CALL :Format
 CALL :Folder
@@ -70,27 +71,30 @@ EXIT /B
 ::
 :URL
 CALL :Space
-SET /P url=Video URL: 
 SET attempt=0
-IF NOT DEFINED retry SET retry=5
-CALL :Space
-IF DEFINED url (
-    IF EXIST "%url%" (
-        FOR /F "usebackq tokens=* delims=" %%I IN ("%url%") DO (CALL :Process "%%I")
-    ) ELSE (
-        CALL :Process "%url%"
-    )
-)
+SET /P url=Video URL: 
+IF "%url%"=="@format" CALL :Clear Format
+IF "%url%"=="@folder" CALL :Clear Folder
+IF "%url%"=="@proxy" CALL :Clear Bypass
+IF "%url%"=="@update" CALL :Updater
+IF EXIST "%url%" FOR /F "usebackq tokens=* delims=" %%I IN ("%url%") DO (CALL :Download "%%I")
+ECHO "%url%" | FIND "https://" && CALL :Download "%url%"
+GOTO :URL
+:Download
+bin\youtube-dl.exe %format% %output% %archive% %server% %1 --verbose && CALL :Finish || CALL :Retry
+CALL :Download %1
+:Updater
+bin\youtube-dl.exe %server% --update --verbose && CALL :Finish || CALL :Retry
+CALL :Updater
+:Retry
+IF "%retry%"=="%attempt%" GOTO :URL
+TIMEOUT /T 5
+SET /A attempt=%attempt%+1
+EXIT /B
+:Finish
+SET url=
 GOTO :URL
 ::
-:Process
-bin\youtube-dl.exe %format% %output% %archive% %server% %1 --verbose || (
-    IF "%retry%"=="%attempt%" GOTO :URL
-    TIMEOUT /T 5
-    SET /A attempt=%attempt%+1
-    GOTO :Process
-)
-EXIT /B
 :Audio
 ECHO Format Option: Audio Only
 SET format=-f "bestaudio"
@@ -123,6 +127,11 @@ ECHO ===========================================================================
 CALL :Warn_%1
 ECHO ========================================================================================
 EXIT /B
+:Clear
+SET url=
+CALL :Clear_%1
+CALL :%1
+EXIT /B
 :Warn_Format
 ECHO 1. Audio Only
 ECHO 2. Best Quality
@@ -130,8 +139,15 @@ ECHO 3. Best Quality @1080p
 ECHO 4. Best Quality @720p
 ECHO 5. Best Quality @480p
 EXIT /B
+:Clear_Format
+SET form=
+SET format=
+EXIT /B
 :Warn_Folder
 ECHO %~DP0download (Default)
+EXIT /B
+:Clear_Folder
+SET dir=
 EXIT /B
 :Warn_Bypass
 ECHO 2. Other
@@ -141,4 +157,7 @@ EXIT /B
 :Warn_Server
 ECHO Keep EMPTY if you don't use a proxy
 ECHO 127.0.0.1:1080 (Sample)
+EXIT /B
+:Clear_Bypass
+SET pass=
 EXIT /B
