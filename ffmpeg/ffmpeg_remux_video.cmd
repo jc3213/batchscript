a3e616d0ba8bd8277eac908c232994ce01e143ad
jc3213 <jc3213@users.noreply.github.com>
2021-05-27 10:03:21 +0800

    use for loop to support more than 9 files

--- ffmpeg/ffmpeg_remux_video.cmd
+++ ffmpeg/ffmpeg_remux_video.cmd
@@ -1,50 +1,13 @@
 @ECHO OFF
-:File1
-IF NOT EXIST "%1" (
-    GOTO :End
-)
-SET input=-i "%1"
-:File2
-IF NOT EXIST "%2" (
+:Files
+IF NOT EXIST "%~1" (
     GOTO :Proc
 )
-SET input=%input% -i "%2"
-:File3
-IF NOT EXIST "%3" (
-    GOTO :Proc
+SET Files=-i "%1" %Files%
+FOR /F "tokens=1,* delims= " %%A IN ("%*") DO (
+    CALL :Files %%B
 )
-SET input=%input% -i "%3"
-:File4
-IF NOT EXIST "%4" (
-    GOTO :Proc
-)
-SET input=%input% -i "%4"
-:File5
-IF NOT EXIST "%5" (
-    GOTO :Proc
-)
-SET input=%input% -i "%5"
-:File6
-IF NOT EXIST "%6" (
-    GOTO :Proc
-)
-SET input=%input% -i "%6"
-:File7
-IF NOT EXIST "%7" (
-    GOTO :Proc
-)
-SET input=%input% -i "%7"
-:File8
-IF NOT EXIST "%8" (
-    GOTO :Proc
-)
-SET input=%input% -i "%8"
-:File9
-IF NOT EXIST "%9" (
-    GOTO :Proc
-)
-SET input=%input% -i "%9"
 :Proc
-"%~DP0bin\ffmpeg.exe" %input% -c:v copy -c:a copy remuxed_%~n1.mkv -threads 128
+"%~DP0bin\ffmpeg.exe" %Files% -c:v copy -c:a copy remuxed_%~n1.mkv -threads 128
 :End
 EXIT
