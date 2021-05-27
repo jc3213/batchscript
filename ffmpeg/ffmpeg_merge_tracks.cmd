006c27e659d5093fd6876b257827f2fafcfa9c85
jc3213 <jc3213@users.noreply.github.com>
2021-05-27 10:16:54 +0800

    some clean up

--- ffmpeg/ffmpeg_merge_tracks.cmd
+++ ffmpeg/ffmpeg_merge_tracks.cmd
@@ -1,12 +1,10 @@
 @ECHO OFF
 SET Index=0
 :Files
-IF NOT EXIST "%~1" (
-    GOTO :Proc
-)
+IF NOT EXIST "%~1" GOTO :Proc
 SET Files=-i "%1" %Files%
 SET /A Index=%Index%+1
-FOR /F "TOKENS=1,* DELIMS= " %%A IN ("%*") DO (CALL :Files %%B)
+FOR /F "TOKENS=1,* DELIMS= " %%A IN ("%*") DO CALL :Files %%B
 :Proc
 IF NOT DEFINED Files GOTO :End
 "%~DP0bin\ffmpeg.exe" %Files% -filter_complex "amerge=inputs=%Index%" merged_audio_track%~x1
