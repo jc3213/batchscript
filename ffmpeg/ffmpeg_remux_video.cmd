8877814b425d4a2109621fa8493a99ff3ad01799
jc3213 <jc3213@users.noreply.github.com>
2021-05-27 12:28:19 +0800

    Update ffmpeg_remux_video.cmd

--- ffmpeg/ffmpeg_remux_video.cmd
+++ ffmpeg/ffmpeg_remux_video.cmd
@@ -1,7 +1,15 @@
 @ECHO OFF
 :Files
 IF NOT EXIST "%~1" GOTO :Proc
-SET Input=-i "%1" %Input%
+"%~DP0bin\ffmpeg.exe" -i "%~1" >%Temp%\ffmpeg_log_%~N1.txt 2>&1
+FOR /F "USEBACKQ SKIP=13 TOKENS=4,5 DELIMS=,: " %%I IN ("%Temp%\ffmpeg_log_%~N1.txt") DO (
+	IF "%%I"=="Audio" (
+        SET Audio=-i "%~1"
+    ) ELSE IF "%%I"=="Video" (
+        SET Video=-i "%~1"
+    )
+)
+SET Input=%Video% %Audio%
 FOR /F "TOKENS=1,* DELIMS= " %%A IN ("%*") DO CALL :Files %%B
 :Proc
 IF NOT DEFINED Input GOTO :End
