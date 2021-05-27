cf3b16a8286178d13189bc8d7a5c73669242f8c8
jc3213 <jc3213@users.noreply.github.com>
2021-05-27 10:51:58 +0800

    typo

--- ffmpeg/ffmpeg_merge_tracks.cmd
+++ ffmpeg/ffmpeg_merge_tracks.cmd
@@ -7,7 +7,7 @@ SET Input=-i "%1" %Input%
 SET /A Index=%Index%+1
 FOR /F "TOKENS=1,* DELIMS= " %%A IN ("%*") DO CALL :Files %%B
 :Proc
-IF NOT DEFINED Files GOTO :End
+IF NOT DEFINED Input GOTO :End
 "%~DP0bin\ffmpeg.exe" %Input% -filter_complex "amerge=inputs=%Index%" merged_audio_track.%File%
 :End
 EXIT
