8d0811b6611dc4a2cbc380ff6f48b01226d41f22
jc3213 <jc3213@users.noreply.github.com>
2021-05-27 10:50:10 +0800

    oops, forget about file extensions

--- ffmpeg/ffmpeg_merge_tracks.cmd
+++ ffmpeg/ffmpeg_merge_tracks.cmd
@@ -1,12 +1,13 @@
 @ECHO OFF
 SET Index=0
+SET File=%~X1
 :Files
 IF NOT EXIST "%~1" GOTO :Proc
-SET Files=-i "%1" %Files%
+SET Input=-i "%1" %Input%
 SET /A Index=%Index%+1
 FOR /F "TOKENS=1,* DELIMS= " %%A IN ("%*") DO CALL :Files %%B
 :Proc
 IF NOT DEFINED Files GOTO :End
-"%~DP0bin\ffmpeg.exe" %Files% -filter_complex "amerge=inputs=%Index%" merged_audio_track%~x1
+"%~DP0bin\ffmpeg.exe" %Input% -filter_complex "amerge=inputs=%Index%" merged_audio_track.%File%
 :End
 EXIT
