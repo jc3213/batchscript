608cb57fd7205a64cce5971c6aa35eea334281eb
jc3213 <jc3213@users.noreply.github.com>
2021-05-27 10:16:41 +0800

    some clean up

--- ffmpeg/ffmpeg_remux_video.cmd
+++ ffmpeg/ffmpeg_remux_video.cmd
@@ -1,13 +1,10 @@
 @ECHO OFF
 :Files
-IF NOT EXIST "%~1" (
-    GOTO :Proc
-)
+IF NOT EXIST "%~1" GOTO :Proc
 SET Files=-i "%1" %Files%
-FOR /F "tokens=1,* delims= " %%A IN ("%*") DO (
-    CALL :Files %%B
-)
+FOR /F "TOKENS=1,* DELIMS= " %%A IN ("%*") DO CALL :Files %%B
 :Proc
+IF NOT DEFINED Files GOTO :End
 "%~DP0bin\ffmpeg.exe" %Files% -c:v copy -c:a copy remuxed_%~n1.mkv -threads 128
 :End
 EXIT
