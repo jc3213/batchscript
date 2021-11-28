3c9159c5f72c7cde2f2124281101a58f967c9241
jc3213 <jc3213@users.noreply.github.com>
2021-11-28 17:46:13 +0800

    get build number not full version

--- Windows/CPU Microcode Update/Remove.cmd
+++ Windows/CPU Microcode Update/Remove.cmd
@@ -1,13 +1,12 @@
 @ECHO OFF
-PUSHD %~DP0
-FOR /F "tokens=2 delims==" %%I IN ('wmic os get Version /value') DO (SET Backup=%%I)
-MD %Backup% 2>NUL
+FOR /F "tokens=2 delims==" %%I IN ('wmic os get BuildNumber /value') DO (SET Backup=%%I)
+MD "%~DP0%Backup%" >NUL
 CALL :Process %SystemRoot%\System32\mcupdate_AuthenticAMD.dll
 CALL :Process %SystemRoot%\System32\mcupdate_GenuineIntel.dll
 GOTO :Exit
 :Process
 %SystemRoot%\System32\cmd.exe /C TAKEOWN /F %1 && ICACLS %1 /grant Administrators:F
-COPY /Y %1 %Backup%
+COPY /Y %1 "%~DP0%Backup%"
 DEL %1 /F /Q
 EXIT /B
 :Exit
--- Windows/CPU Microcode Update/Restore.cmd
+++ Windows/CPU Microcode Update/Restore.cmd
@@ -1,6 +1,5 @@
 @ECHO OFF
-PUSHD %~DP0
-FOR /F "tokens=2 delims==" %%I IN ('wmic os get Version /value') DO (SET Backup=%%I)
-COPY /Y %Backup%\mcupdate_AuthenticAMD.dll %SystemRoot%\System32
-COPY /Y %Backup%\mcupdate_GenuineIntel.dll %SystemRoot%\System32
+FOR /F "tokens=2 delims==" %%I IN ('wmic os get BuildNumber /value') DO (SET Backup=%%I)
+COPY /Y "%~DP0%Backup%\mcupdate_AuthenticAMD.dll" %SystemRoot%\System32
+COPY /Y "%~DP0%Backup%\mcupdate_GenuineIntel.dll" %SystemRoot%\System32
 TIMEOUT /T 5
