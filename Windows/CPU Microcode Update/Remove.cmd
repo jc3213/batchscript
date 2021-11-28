6747aee49456a7469cf43fcd9d8f095aa818d22c
jc3213 <jc3213@users.noreply.github.com>
2021-11-28 19:31:33 +0800

    just use backup folder instead of build number

--- Windows/CPU Microcode Update/Remove.cmd
+++ Windows/CPU Microcode Update/Remove.cmd
@@ -1,13 +1,12 @@
 @ECHO OFF
-FOR /F "tokens=2 delims==" %%I IN ('wmic os get BuildNumber /value') DO (SET Backup=%%I)
-MD "%~DP0%Backup%" >NUL
+PUSHD %~DP0
+MD Backup >NUL
 CALL :Process %SystemRoot%\System32\mcupdate_AuthenticAMD.dll
 CALL :Process %SystemRoot%\System32\mcupdate_GenuineIntel.dll
-GOTO :Exit
+TIMEOUT /T 5
+EXIT
 :Process
 %SystemRoot%\System32\cmd.exe /C TAKEOWN /F %1 && ICACLS %1 /grant Administrators:F
-COPY /Y %1 "%~DP0%Backup%"
+COPY /Y %1 Backup
 DEL %1 /F /Q
 EXIT /B
-:Exit
-TIMEOUT /T 5
--- Windows/CPU Microcode Update/Restore.cmd
+++ Windows/CPU Microcode Update/Restore.cmd
@@ -1,5 +1,5 @@
 @ECHO OFF
-FOR /F "tokens=2 delims==" %%I IN ('wmic os get BuildNumber /value') DO (SET Backup=%%I)
-COPY /Y "%~DP0%Backup%\mcupdate_AuthenticAMD.dll" %SystemRoot%\System32
-COPY /Y "%~DP0%Backup%\mcupdate_GenuineIntel.dll" %SystemRoot%\System32
+PUSHD %~DP0
+COPY /Y Backup\mcupdate_AuthenticAMD.dll %SystemRoot%\System32
+COPY /Y Backup\mcupdate_GenuineIntel.dll %SystemRoot%\System32
 TIMEOUT /T 5
