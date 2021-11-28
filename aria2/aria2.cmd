bbafd4d3965437008d2d7af9b856d51829d7781e
jc3213 <jc3213@users.noreply.github.com>
2021-11-28 21:13:37 +0800

    optimized logic and performance

--- aria2/aria2.cmd
+++ aria2/aria2.cmd
@@ -1,54 +1,27 @@
 @ECHO OFF
 PUSHD %~DP0
-IF "%1"=="/h" CALL :RunHide
-IF "%1"=="/n" CALL :Restart
-IF "%1"=="/nh" CALL :Restart Hide
+IF "%1"=="/h" CALL :Run 0
 IF "%1"=="/s" CALL :Session
-IF "%1"=="/sh" CALL :Session Hide
-IF "%1"=="/r" CALL :Register
-IF "%1"=="/rh" CALL :Register Hide
+IF "%1"=="/sh" CALL :Session 0
+IF "%1"=="/r" CALL :Startup
+IF "%1"=="/rh" CALL :Startup 0
 IF "%1"=="/u" CALL :Unregister
-:Run
-IF NOT EXIST "%CD%\aria2.session" (
-    CALL :CleanSession
-)
-"%CD%\bin\aria2c.exe" --conf="%CD%\aria2.conf"
-EXIT
-:RunHide
-IF NOT EXIST "%CD%\aria2.vbs" (
-    ECHO CreateObject^("Wscript.Shell"^).Run """%CD%\aria2.cmd""", ^0>aria2.vbs
-)
-"%CD%\aria2.vbs"
-EXIT
-:Restart
-CALL :Kill
-CALL :Run%1
-:Register
-IF "%1" == "" (
-    CALL :Registry \"%CD%\aria2.cmd\"
-) ELSE (
-    CALL :Registry \"%CD%\aria2.vbs\"
-)
-CALL :Kill
-CALL :WebUI
-CALL :Run%1
-:Unregister
-CALL :Kill
-REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /V "Aria2" /F
-EXIT
+CALL :Run
 :Session
-CALL :Kill
-CALL :CleanSession
-CALL :Run%1
-:Kill
-TASKKILL /F /IM "aria2c.exe" 2>nul
-EXIT /B
-:WebUI
+ECHO OFF>aria2.session"
+GOTO :Run
+:Startup
+IF "%1"=="0" (SET App=vbs) ELSE (SET App=cmd)
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /V "Aria2" /T "REG_SZ" /D "%CD%\aria2.%App%" /F
+GOTO :Run
+:Run
+TASKKILL /F /IM "aria2c.exe" 1>NUL 2>NUL
+IF "%1"=="0" (
+    IF NOT EXIST aria2.vbs (ECHO CreateObject^("Wscript.Shell"^).Run """%CD%\bin\aria2c.exe"" --conf=""%CD%\aria2.conf""", ^0>aria2.vbs)
+    aria2.vbs
+) ELSE (bin\aria2c.exe --conf=aria2.conf)
 START "" "https://ziahamza.github.io/webui-aria2/"
-EXIT /B
-:Registry
-REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /V "Aria2" /T "REG_SZ" /D "%1" /F
-EXIT /B
-:CleanSession
-ECHO OFF>"%CD%\aria2.session"
-EXIT /B
+EXIT
+:Unregister
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /V "Aria2" /F
+TIMEOUT -T 5
