37375769731f5eff49547478811ae44e833047e9
jc3213 <jc3213@users.noreply.github.com>
2021-12-17 12:09:50 +0800

    minor update

--- Notepad++/XMLTools/Install.cmd
+++ Notepad++/XMLTools/Install.cmd
@@ -1,6 +1,6 @@
 @ECHO OFF
 FOR /F "tokens=1,2*" %%I IN ('REG QUERY HKLM\SOFTWARE\Notepad++ /VE') DO (IF "%%I"=="(Default)" SET Folder=%%K\plugins\XMLTools)
-FOR /F "TOKENS=*" %%I IN ('DIR %~DP0*.zip /B /OD') DO (SET Zip=%~DP0%%I)
+FOR /F "tokens=*" %%I IN ('DIR %~DP0XMLTools-*.zip /B /OD') DO (SET Zip=%~DP0%%I)
 MD "%Folder%" 2>NUL
 %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Expand-Archive -Force -Path '%Zip%' -DestinationPath '%Folder%'"
 TIMEOUT /T 5
