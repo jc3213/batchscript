6506a181ddcafe3add2bf8d37b493132ac414646
jc3213 <jc3213@users.noreply.github.com>
2021-12-17 14:25:54 +0800

    minor update

--- Windows/CPU Microcode Update/Remove.cmd
+++ Windows/CPU Microcode Update/Remove.cmd
@@ -2,5 +2,5 @@
 %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Compress-Archive -Path '%SystemRoot%\System32\mcupdate_AuthenticAMD.dll','%SystemRoot%\System32\mcupdate_GenuineIntel.dll' -DestinationPath '%~DP0Backup.zip'"
 %SystemRoot%\System32\cmd.exe /C TAKEOWN /F "%SystemRoot%\System32\mcupdate_AuthenticAMD.dll" && ICACLS "%SystemRoot%\System32\mcupdate_AuthenticAMD.dll" /grant Administrators:F
 %SystemRoot%\System32\cmd.exe /C TAKEOWN /F "%SystemRoot%\System32\mcupdate_GenuineIntel.dll" && ICACLS "%SystemRoot%\System32\mcupdate_GenuineIntel.dll" /grant Administrators:F
-DEL %SystemRoot%\System32\mcupdate_AuthenticAMD.dll %SystemRoot%\System32\mcupdate_GenuineIntel.dll /F /Q
+DEL "%SystemRoot%\System32\mcupdate_AuthenticAMD.dll" "%SystemRoot%\System32\mcupdate_GenuineIntel.dll" /F /Q
 TIMEOUT /T 5
