b052470335ebebd3379e82e90a4f285541029d68
jc3213 <jc3213@users.noreply.github.com>
2022-06-21 07:25:33 +0800

    optimized logic

--- aria2/aria2.cmd
+++ aria2/aria2.cmd
@@ -12,12 +12,14 @@ ECHO OFF>aria2.session"
 GOTO :Run
 :Startup
 IF "%1"=="0" (SET App=vbs) ELSE (SET App=cmd)
-REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /V "Aria2" /T "REG_SZ" /D "%CD%\aria2.%App%" /F
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /V "aria2" /T "REG_SZ" /D "%CD%\aria2.%App%" /F
 GOTO :Run
 :Run
-TASKKILL /F /IM "aria2c.exe" 1>NUL 2>NUL
 IF "%1"=="0" (
-    IF NOT EXIST aria2.vbs (ECHO CreateObject^("Wscript.Shell"^).Run """%CD%\bin\aria2c.exe"" --conf=""%CD%\aria2.conf""", ^0>aria2.vbs)
+    IF NOT EXIST aria2.vbs (
+        ECHO Set Sh=CreateObject^("WScript.Shell"^)>aria2.vbs
+        ECHO CreateObject^("Wscript.Shell"^).Run Sh.CurrentDirectory & "\aria2.cmd", ^0>>aria2.vbs
+    )
     aria2.vbs
 ) ELSE (bin\aria2c.exe --conf=aria2.conf)
 START "" "https://ziahamza.github.io/webui-aria2/"
