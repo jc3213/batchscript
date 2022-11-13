c81bb74d561eb1c9149fa97bf4690a9f128ce293
jc3213 <jc3213@users.noreply.github.com>
2022-11-13 16:05:47 +0800

    Update aria2c.cmd

--- aria2c/aria2c.cmd
+++ aria2c/aria2c.cmd
@@ -6,24 +6,23 @@ IF "%1"=="/sh" CALL :Session 0
 IF "%1"=="/r" CALL :Startup
 IF "%1"=="/rh" CALL :Startup 0
 IF "%1"=="/u" CALL :Unregister
-CALL :Run
-:Session
-ECHO OFF>aria2.session"
 GOTO :Run
 :Startup
 IF "%1"=="0" (SET App=vbs) ELSE (SET App=cmd)
-REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /V "aria2" /T "REG_SZ" /D "%CD%\aria2.%App%" /F
-GOTO :Run
+REG ADD "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /V "aria2c" /T "REG_SZ" /D "%CD%\aria2c.%App%" /F
+START "" "https://ziahamza.github.io/webui-aria2/"
 :Run
 IF "%1"=="0" (
-    IF NOT EXIST aria2.vbs (
-        ECHO Set Sh=CreateObject^("WScript.Shell"^)>aria2.vbs
-        ECHO CreateObject^("Wscript.Shell"^).Run Sh.CurrentDirectory & "\aria2.cmd", ^0>>aria2.vbs
+    IF NOT EXIST aria2c.vbs (
+        ECHO Set Sh=CreateObject^("WScript.Shell"^)>aria2c.vbs
+        ECHO CreateObject^("Wscript.Shell"^).Run Sh.CurrentDirectory & "\aria2c.cmd", ^0>>aria2c.vbs
     )
     aria2.vbs
-) ELSE (bin\aria2c.exe --conf=aria2.conf)
-START "" "https://ziahamza.github.io/webui-aria2/"
+) ELSE (bin\aria2c.exe --conf=aria2c.conf)
 EXIT
+:Session
+ECHO OFF>aria2c.session"
+GOTO :Run
 :Unregister
-REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /V "Aria2" /F
+REG DELETE "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /V "aria2c" /F
 TIMEOUT -T 5
