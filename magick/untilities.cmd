563ca96194acf84b46575d90434ac47fcd85bd03
jc3213 <jc3213@users.noreply.github.com>
2022-11-21 22:54:46 +0800

    Create untilities.cmd

--- magick/untilities.cmd
+++ magick/untilities.cmd
@@ -7,22 +7,24 @@ CD /D %1 2>NUL
 IF %ErrorLevel% EQU 0 (
     ECHO 1^) Cut Image Border
     ECHO 2^) Convert to JPG
+    ECHO 3^) Compress as ZIP
 ) ELSE (
-    ECHO 0^) Repack Zip File
+    ECHO 1^) Repack to new ZIP
     SET File=%1
 )
 ECHO ============================================================
 :What
 SET /P ACT=^> 
+ECHO.
 IF NOT DEFINED File (
     IF %ACT% EQU 1 GOTO :Cut
     IF %ACT% EQU 2 GOTO :JPG
+    IF %ACT% EQU 3 GOTO :ZIP
 ) ELSE (
-    IF %ACT% EQU 0 GOTO :Unzip
+    IF %ACT% EQU 1 GOTO :Repack
 )
 GOTO :What
 :Cut
-ECHO.
 ECHO.
 ECHO ============================================================
 ECHO https://imagemagick.org/script/command-line-processing.php#geometry
@@ -31,7 +33,6 @@ ECHO Cut left and right: 300px(width), cut top and bottom: 100px(height)
 ECHO ============================================================
 SET /P Opt=^> 
 IF NOT DEFINED Opt GOTO :Cut
-ECHO.
 ECHO.
 ECHO ============================================================
 ECHO ImageMagick is cutting images...
@@ -42,7 +43,6 @@ FOR %%I IN (*) DO ("%~DP0bin\magick.exe" convert %%I -shave %Opt% "%Folder%\%%I"
 GOTO :End
 :JPG
 ECHO.
-ECHO.
 ECHO ============================================================
 ECHO ImageMagick is converting images...
 ECHO ============================================================
@@ -50,8 +50,7 @@ SET Folder=%~DP1conv_%~N1
 MD "%Folder%" 2>NUL
 FOR %%I IN (*) DO ("%~DP0bin\magick.exe" %%I "%Folder%\%%~NI.jpg")
 GOTO :End
-:Unzip
-ECHO.
+:Repack
 ECHO.
 ECHO ============================================================
 ECHO Repacking to new Zip...
