aa9a25e1ea64137d39dc8fd0ef841d245099d487
jc3213 <jc3213@users.noreply.github.com>
2022-12-03 23:07:02 +0800

    support crop

--- magick/untilities.cmd
+++ magick/untilities.cmd
@@ -5,9 +5,10 @@ SET Name=%~N1
 ECHO ============================================================
 CD /D %1 2>NUL
 IF %ErrorLevel% EQU 0 (
-    ECHO 1^) Cut Image Border
-    ECHO 2^) Convert to JPG
-    ECHO 3^) Compress as ZIP
+    ECHO 1^) Crop Image Area
+    ECHO 2^) Cut Image Border
+    ECHO 3^) Convert to JPG
+    ECHO 4^) Compress as ZIP
 ) ELSE (
     ECHO 1^) Repack to new ZIP
     SET File=%1
@@ -17,26 +18,22 @@ ECHO ============================================================
 SET /P ACT=^> 
 ECHO.
 IF NOT DEFINED File (
-    IF %ACT% EQU 1 GOTO :Cut
-    IF %ACT% EQU 2 GOTO :JPG
-    IF %ACT% EQU 3 GOTO :ZIP
+    IF %ACT% EQU 1 GOTO :Crop
+    IF %ACT% EQU 2 GOTO :Shave
+    IF %ACT% EQU 3 GOTO :JPG
+    IF %ACT% EQU 4 GOTO :ZIP
 ) ELSE (
     IF %ACT% EQU 1 GOTO :Repack
 )
 GOTO :What
-:Cut
-ECHO.
-ECHO ============================================================
-ECHO https://imagemagick.org/script/command-line-processing.php#geometry
-ECHO Sample: 300x100 (width x height)
-ECHO Cut left and right: 300px(width), cut top and bottom: 100px(height)
-ECHO ============================================================
-SET /P Opt=^> 
-IF NOT DEFINED Opt GOTO :Cut
-ECHO.
-ECHO ============================================================
-ECHO ImageMagick is cutting images...
-ECHO ============================================================
+:Crop
+CALL :Front
+SET Folder=%~DP1cutted_%Name%
+MD "%Folder%" 2>NUL
+FOR %%I IN (*) DO ("%~DP0bin\magick.exe" convert %%I -crop %Opt% "%Folder%\%%I")
+GOTO :End
+:Shave
+CALL :Front
 SET Folder=%~DP1cutted_%Name%
 MD "%Folder%" 2>NUL
 FOR %%I IN (*) DO ("%~DP0bin\magick.exe" convert %%I -shave %Opt% "%Folder%\%%I")
@@ -56,8 +53,9 @@ ECHO ============================================================
 ECHO Repacking to new Zip...
 ECHO ============================================================
 SET Folder=%~N1
-ECHO a|"%~DP0bin\7za.exe" x "%~DPN1.zip" -o"%Folder%"
-CD %Folder%
+ECHO a|"%~DP0bin\7za.exe" x %1 -o"%Folder%"
+CD %Folder% 2>NUL
+IF %ErrorLevel% NEQ 0 GOTO :Exit
 "%~DP0bin\7za.exe" a "%~DPN1.zip" "*"
 CD..
 RD /S /Q "%Folder%"
@@ -83,6 +81,23 @@ IF %Cfm% NEQ y GOTO :Exit
 CD..
 RD /S /Q %1 2>NUL
 RD /S /Q "%Folder%" 2>NUL
-TIMEOUT -T 5
 :Exit
+TIMEOUT -T 5
 EXIT
+:Front
+ECHO.
+ECHO ============================================================
+ECHO https://imagemagick.org/script/command-line-processing.php#geometry
+ECHO Sample: 300x100 (width x height)
+ECHO Cut left and right: 300px(width), cut top and bottom: 100px(height)
+ECHO Sample: 300x100+20+30 (width x height + left + top)
+ECHO Crop image area start from: left 20px to 320px, top: 30px to 130px
+ECHO ============================================================
+SET /P Opt=^> 
+IF NOT DEFINED Opt GOTO :Opt
+ECHO.
+ECHO.
+ECHO ============================================================
+ECHO ImageMagick is cutting images...
+ECHO ============================================================
+EXIT /B
