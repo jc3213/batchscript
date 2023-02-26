29a3289a0fd0530a2a9854718d9b6e7580cefd81
jc3213 <jc3213@users.noreply.github.com>
2023-02-26 13:40:28 +0800

    Update Alt+Tab switch window only.cmd

--- Windows/Microsoft Edge/Alt+Tab switch window only.cmd
+++ Windows/Microsoft Edge/Alt+Tab switch window only.cmd
@@ -1,3 +1,3 @@
 @ECHO OFF
-REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /V "MultiTaskingAltTabFilter" /T "REG_DWORD" /D "0x00000001" /F
+REG ADD "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" /V "MultiTaskingAltTabFilter" /T "REG_DWORD" /D "0x00000003" /F
 TIMEOUT /T 5
