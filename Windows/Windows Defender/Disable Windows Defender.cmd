36b5ad1c861657a00c7ed2052e66f0853cbc5e09
jc3213 <jc3213@users.noreply.github.com>
2023-03-29 08:08:54 +0800

    Update Windows Defender

--- Windows/Windows Defender/Disable Windows Defender.cmd
+++ Windows/Windows Defender/Disable Windows Defender.cmd
@@ -1,7 +1,12 @@
 @ECHO OFF
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /V "DisableAntiSpyware" /T "REG_DWORD" /D "0x00000001" /F
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /V "DisableRealtimeMonitoring" /T "REG_DWORD" /D "0x00000001" /F
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /V "DisableBehaviorMonitoring" /T "REG_DWORD" /D "0x00000001" /F
+REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /V "DisableScanOnRealtimeEnable" /T "REG_DWORD" /D "0x00000001" /F
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Virus and threat protection" /V "UILockdown" /T "REG_DWORD" /D "0x00000001" /F
 REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Systray" /V "HideSystray" /T "REG_DWORD" /D "0x00000001" /F
+REG ADD "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /V "SecurityHealth" /T "REG_BINARY" /D "07000000CD54F699D161D900" /F
+REG DELETE "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /V "SecurityHealth" /F
 REG DELETE "HKCR\*\shellex\ContextMenuHandlers\EPP" /VE /F
 REG DELETE "HKCR\Drive\shellex\ContextMenuHandlers\EPP" /VE /F
 REG DELETE "HKCR\Directory\shellex\ContextMenuHandlers\EPP" /VE /F
