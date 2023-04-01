dd50723126235a8efb05648481edb2324e4b2c64
jc3213 <jc3213@users.noreply.github.com>
2023-04-01 21:32:45 +0800

    Update zip_for_comic.cmd

--- 7-zip/zip_for_comic.cmd
+++ 7-zip/zip_for_comic.cmd
@@ -1,35 +1,34 @@
 @ECHO OFF
 PUSHD %~DP0
-IF EXIST "%ProgramFiles%\7-Zip\7z.exe" SET Zip=%ProgramFiles%\7-Zip\7z.exe
-IF DEFINED Zip GOTO :Main
-IF EXIST "%CD%\bin\7za.exe" SET Zip=%CD%\bin\7za.exe
-IF NOT DEFINED Zip GOTO :Exit
+SET Zip=%ProgramFiles%\7-Zip\7z.exe
+IF EXIST "%Zip%" GOTO :Main
+SET Zip=%CD%\bin\7za.exe
+IF NOT EXIST "%Zip%" GOTO :Exit
 :Main
 ECHO ==================================================================
-ECHO Remove original files or temporary files? (Y/N)
+ECHO Remove original files or temporary files? (Y/y)
 ECHO ==================================================================
 SET /P Yes=^> 
 FOR %%I IN (%*) DO (CALL :Core %%I)
 GOTO :Exit
 :Core
-IF NOT EXIST "%1" GOTO :Exit
 CD /D %1
 IF %ErrorLevel% EQU 0 GOTO :NewPack
 :Repack
 ECHO a|"%Zip%" x %1 -o"%~DPN1"
 "%Zip%" a "%~DPN1.zip" "%~DPN1\*"
-IF %Yes% EQU Y GOTO :ReZip
-IF %Yes% EQU y GOTO :ReZip
+IF %Yes% EQU Y GOTO :ReClear
+IF %Yes% EQU y GOTO :ReClear
 EXIT /B
-:ReZip
+:ReClear
 RD /S /Q "%~DPN1"
 EXIT /B
 :NewPack
 "%Zip%" a "%~DPNX1.zip" "*"
-IF %Yes% EQU Y GOTO :NewZip
-IF %Yes% EQU y GOTO :NewZip
+IF %Yes% EQU Y GOTO :NewClear
+IF %Yes% EQU y GOTO :NewClear
 EXIT /B
-:NewZip
+:NewClear
 CD..
 RD /S /Q "%~1"
 EXIT /B
