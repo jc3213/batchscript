a1543411c9e8120c61459f069d6d6261581d691c
jc3213 <jc3213@users.noreply.github.com>
2023-04-05 23:43:02 +0800

    check system and use curl to download itunes

--- 7-zip/itunes_aac_libs.cmd
+++ 7-zip/itunes_aac_libs.cmd
@@ -2,30 +2,28 @@
 PUSHD %~DP0
 FOR /F "tokens=1,2*" %%I IN ('REG QUERY HKLM\Software\7-Zip /V Path') DO (IF "%%I"=="Path" SET Zip=%%K7z.exe)
 IF NOT EXIST "%Zip%" GOTO :Exit
-:Main
-IF NOT EXIST "%~1" GOTO :Input
-SET File=%1
-SET Name=%~N1
-GOTO :Unpack
-:Input
-SET /P File=Select iTunes Installer: 
-IF NOT EXIST "%File%" GOTO :Input
-CALL :File %File%
-:Unpack
-SET Name=%Name:Setup=%
-SET Pack=%Name%.msi
+IF %PROCESSOR_ARCHITECTURE% EQU AMD64 (SET Arch=64) ELSE (SET Arch=32)
+SET File=%~NX1
+IF EXIST "%File%" GOTO :Extractor
+SET File=iTunes%Arch%Setup.exe
+IF EXIST %File% GOTO :Extractor
+CURL https://www.apple.com/itunes/download/win%Arch% --location --output %File%
+:Extractor
+SET Output=iTunes%Arch%
+SET iTunes=iTunes%Arch%.msi
 SET Unpack=%CD%\.Unpacked
-"%Zip%" e -y "%File%" %Pack%
-MD %Name% 2>NUL
-MSIEXEC /A %Pack% /Q TARGETDIR=%Unpack%
-COPY %Unpack%\iTunes\ASL.dll %Name%
-COPY %Unpack%\iTunes\CoreAudioToolbox.dll %Name%
-COPY %Unpack%\iTunes\CoreFoundation.dll %Name%
-COPY %Unpack%\iTunes\icudt*.dll %Name%
-COPY %Unpack%\iTunes\libdispatch.dll %Name%
-COPY %Unpack%\iTunes\libicuin.dll %Name%
-COPY %Unpack%\iTunes\libicuuc.dll %Name%
-COPY %Unpack%\iTunes\objc.dll %Name%
+MD %Unpack% 2>NUL
+MD %Output% 2>NUL
+"%Zip%" e -y %File% %iTunes%
+MSIEXEC /A %iTunes% /QN TARGETDIR="%Unpack%"
+COPY %Unpack%\iTunes\ASL.dll %Output%
+COPY %Unpack%\iTunes\CoreAudioToolbox.dll %Output%
+COPY %Unpack%\iTunes\CoreFoundation.dll %Output%
+COPY %Unpack%\iTunes\icudt*.dll %Output%
+COPY %Unpack%\iTunes\libdispatch.dll %Output%
+COPY %Unpack%\iTunes\libicuin.dll %Output%
+COPY %Unpack%\iTunes\libicuuc.dll %Output%
+COPY %Unpack%\iTunes\objc.dll %Output%
 RD %Unpack% /S /Q 2>NUL
 DEL %Pack% 2>NUL
 :Exit
