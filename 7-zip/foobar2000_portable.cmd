71b049381e91e4528222e4b888eac5b7cd6eb3b4
jc3213 <jc3213@users.noreply.github.com>
2023-04-09 22:52:02 +0800

    Update foobar2000_portable.cmd

--- 7-zip/foobar2000_portable.cmd
+++ 7-zip/foobar2000_portable.cmd
@@ -2,24 +2,12 @@
 PUSHD %~DP0
 FOR /F "tokens=1,2*" %%I IN ('REG QUERY HKLM\Software\7-Zip /V Path') DO (IF "%%I"=="Path" SET Zip=%%K7z.exe)
 IF NOT EXIST "%Zip%" GOTO :Exit
-ECHO ==================================================================
-ECHO Extracting files from %~NX1
-ECHO ==================================================================
-ECHO.
 "%Zip%" x %1 -y -x!$PLUGINSDIR -x!$R0 -x!uninstall.exe -o"%~DPN1"
-ECHO.
-TIMEOUT -T 1
-ECHO.
-ECHO ==================================================================
-ECHO Makes Foobar2000 portable
-ECHO ==================================================================
-ECHO.
-TIMEOUT -T 1
 TYPE NUL > "%~DPN1\portable_mode_enabled"
 ECHO.
-ECHO ==================================================================
+ECHO.
 ECHO Portable location %~DPN1
-ECHO ==================================================================
+ECHO.
 ECHO.
 TIMEOUT -T 5
 EXIT
