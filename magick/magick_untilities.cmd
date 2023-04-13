31207365bc53404f9b4f0661af7e6c7f0e41cbf7
jc3213 <jc3213@users.noreply.github.com>
2023-04-13 16:40:36 +0800

    Update magick_untilities.cmd

--- magick/magick_untilities.cmd
+++ magick/magick_untilities.cmd
@@ -1,96 +1,31 @@
 @ECHO OFF
 TITLE ImageMagick Utilities
-SET Zip=%~DP0bin\7za.exe
-CD /D %1 2>NUL
-IF %ErrorLevel% EQU 0 GOTO :DirOpt
-IF %~X1 EQU .zip GOTO :ZipOpt
-GOTO :Exit
-:DirOpt
+SET Magick=%~DP0bin\magick.exe
+:Option
 ECHO ============================================================
-ECHO 1. Crop Image Area
-ECHO 2. Cut Image Border
-ECHO 3. Convert to JPG
-ECHO 4. Convert to AVIF
-ECHO 5. Compress as ZIP
+ECHO 1. Crop with area
+ECHO 2. Cut off border
+ECHO 3. Convert format
 ECHO ============================================================
-:DirAct
 SET /P Act=^> 
 IF %Act% EQU 1 GOTO :Crop
 IF %Act% EQU 2 GOTO :Shave
-IF %Act% EQU 3 GOTO :JPG
-IF %Act% EQU 4 GOTO :AVIF
-IF %Act% EQU 5 GOTO :ComZip
-GOTO :DirAct
-:ZipOpt
-ECHO ============================================================
-ECHO 1. Repack to new ZIP
-ECHO ============================================================
-:ZipAct
-SET /P Act=^> 
-IF %Act% EQU 1 GOTO :Repack
-GOTO :ZipAct
+IF %Act% EQU 3 GOTO :Conv
+CLS && GOTO :Option
 :Crop
 CALL :Area
-SET Folder=%~DP1cutted_%~NX1
-MD "%Folder%" 2>NUL
-FOR %%I IN (*) DO ("%~DP0bin\magick.exe" convert %%I -crop %Area% "%Folder%\%%I")
-GOTO :Compress
+FOR %%I IN (%*) DO (CALL :Process %%I crop)
+GOTO :Exit
 :Shave
 CALL :Area
-SET Folder=%~DP1cutted_%~NX1
-MD "%Folder%" 2>NUL
-FOR %%I IN (*) DO ("%~DP0bin\magick.exe" convert %%I -shave %Area% "%Folder%\%%I")
-GOTO :Compress
-:JPG
-SET Folder=%~DP1conv_%~NX1
-Call :Convert
-FOR %%I IN (*) DO ("%~DP0bin\magick.exe" %%I "%Folder%\%%~NI.jpg")
-GOTO :Compress
-:AVIF
-SET Folder=%~DP1conv_%~NX1
-Call :Convert
-FOR %%I IN (*) DO ("%~DP0bin\magick.exe" %%I "%Folder%\%%~NI.avif")
-GOTO :Compress
-:Compress
-ECHO.
-ECHO.
-ECHO ============================================================
-ECHO Compress as Zip file? (y)
-ECHO ============================================================
-SET /P Com=^> 
-IF %Com% NEQ y GOTO :Remove
-:ComZip
-IF NOT DEFINED Folder SET Folder=%~1
-"%Zip%" a "%~DPNX1.zip" "%Folder%\*"
-:Remove
-ECHO.
-ECHO.
-ECHO ============================================================
-ECHO Remove original files? (y)
-ECHO ============================================================
-SET /P Del=^> 
-IF %Del% NEQ y GOTO :Exit
-CD..
-RD /S /Q %1 2>NUL
-RD /S /Q "%Folder%" 2>NUL
+FOR %%I IN (%*) DO (CALL :Process %%I shave)
 GOTO :Exit
-:Repack
-ECHO.
-ECHO ============================================================
-ECHO Repacking to new Zip...
-ECHO ============================================================
-SET Folder=%~N1
-ECHO a|"%Zip%" x %1 -o"%Folder%"
-CD %Folder% 2>NUL
-IF %ErrorLevel% NEQ 0 GOTO :Exit
-"%Zip%" a "%~DPN1.zip" "*"
-CD..
-RD /S /Q "%Folder%"
+:Conv
+CALL :Format
+FOR %%I IN (%*) DO (CALL :Convert %%I)
 GOTO :Exit
-:Exit
-TIMEOUT -T 5
-EXIT
 :Area
+ECHO.
 ECHO.
 ECHO ============================================================
 ECHO https://imagemagick.org/script/command-line-processing.php#geometry
@@ -104,13 +39,44 @@ IF NOT DEFINED Area GOTO :Area
 ECHO.
 ECHO.
 ECHO ============================================================
-ECHO ImageMagick is cutting images...
+ECHO ImageMagick is processing images...
 ECHO ============================================================
+ECHO.
 EXIT /B
-:Convert
+:Process
+CD /D %1 2>NUL
+IF %ErrorLevel% EQU 0 (
+    MD "%~DP1cutted_%~NX1" 2>NUL
+    FOR %%I IN (*) DO ("%Magick%" convert "%%I" -%2 %Area% "%~DP1cutted_%~NX1\%%I")
+    CD..
+) ELSE (
+    "%Magick%" convert %1 -%2 %Area% "%~DP1cutted_%~NX1"
+)
+EXIT /B
+:Format
+ECHO.
 ECHO.
 ECHO ============================================================
-ECHO ImageMagick is converting images...
+ECHO 1. jpg
+ECHO 2. png
+ECHO 3. avif
 ECHO ============================================================
-MD "%Folder%" 2>NUL
+SET /P FM=^> 
+IF %FM% EQU 1 SET Format=jpg
+IF %FM% EQU 2 SET Format=png
+IF %FM% EQU 3 SET Format=avif
+IF NOT DEFINED Format GOTO :Format
+ECHO.
 EXIT /B
+:Convert
+CD /D %1 2>NUL
+IF %ErrorLevel% EQU 0 (
+    MD "%~DP1conv_%~NX1" 2>NUL
+    FOR %%I IN (*) DO ("%Magick%" "%%I" "%~DP1conv_%~NX1\%%~NI.%Format%")
+    CD..
+) ELSE (
+    "%Magick%" %1 "%~DP1conv_%~N1.%Format%"
+)
+EXIT /B
+:Exit
+TIMEOUT -T 5
