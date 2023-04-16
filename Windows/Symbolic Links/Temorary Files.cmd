5674a6839d758332e09aa8029955d86059a3d446
jc3213 <jc3213@users.noreply.github.com>
2023-04-16 10:52:19 +0800

    move temp files to disk volume "ramdisk"

--- Windows/Symbolic Links/Temorary Files.cmd
+++ Windows/Symbolic Links/Temorary Files.cmd
@@ -1,15 +1,7 @@
 @ECHO OFF
-:Drive
-ECHO ====================================================
-ECHO Please enter the ramdisk label for temporary files
-ECHO ====================================================
-SET /P Label=^>
-IF NOT DEFINED Label CLS && GOTO :Drive
-SET Drive=%Label::=%
-:Temp
+FOR /F %%I in ('wmic logicaldisk where "VolumeName='RAMDISK'" get Caption ^| find ":"') DO SET Ramdisk=%%I\Temp
 RD /S /Q "%LocalAppData%\Temp"
 RD /S /Q "%SystemRoot%\Temp"
-MD %Drive%:\Temp 2>NUL
-MKLINK /D "%LocalAppData%\Temp" "%Drive%:\Temp"
-MKLINK /D "%SystemRoot%\Temp" "%Drive%:\Temp"
+MKLINK /D "%LocalAppData%\Temp" "%Ramdisk%"
+MKLINK /D "%SystemRoot%\Temp" "%Ramdisk%"
 TIMEOUT /T 5
