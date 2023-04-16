fd0aa40700d7080d2e212e5d7c6a1c15b4d24fdb
jc3213 <jc3213@users.noreply.github.com>
2023-04-16 10:07:02 +0800

    auto detect ramdisk with label "ramdisk"

--- QQ/move_temp_to_ramdisk.cmd
+++ QQ/move_temp_to_ramdisk.cmd
@@ -1,4 +1,5 @@
 @ECHO OFF
+FOR /F %%I in ('wmic logicaldisk where "VolumeName='RAMDISK'" get Caption ^| find ":"') DO SET Ramdisk=%%I\Temp
 SET Profile=%Public%\Documents\Tencent\QQ\UserDataInfo.ini
 IF NOT EXIST "%Profile%" GOTO :Document
 FOR /F "TOKENS=1,2 DELIMS==" %%I IN ('type %Profile%') DO (
@@ -20,17 +21,17 @@ RD /S /Q Logs 2>NUL
 RD /S /Q QQTempSys 2>NUL
 RD /S /Q QQ\Temp 2>NUL
 RD /S /Q QQ\webkit_cache 2>NUL
-MKLINK /D Logs "R:\Temp"
-MKLINK /D QQTempSys "R:\Temp"
-MKLINK /D QQ\Temp "R:\Temp"
-MKLINK /D QQ\webkit_cache "R:\Temp"
+MKLINK /D Logs %Ramdisk%
+MKLINK /D QQTempSys %Ramdisk%
+MKLINK /D QQ\Temp %Ramdisk%
+MKLINK /D QQ\webkit_cache %Ramdisk%
 CD Users
 FOR /D %%I IN (*) DO (CALL :AppData "%%I")
 GOTO :Exit
 :AppData
 CD %1
 RD /S /Q QQ\WinTemp 2>NUL
-MKLINK /D QQ\WinTemp "R:\temp"
+MKLINK /D QQ\WinTemp %Ramdisk%
 CD..
 EXIT /B
 :Profile
@@ -39,17 +40,17 @@ RD /S /Q Audio 2>NUL
 RD /S /Q FileRecv 2>NUL
 RD /S /Q Image 2>NUL
 RD /S /Q Video 2>NUL
-MKLINK /D Audio "R:\Temp"
-MKLINK /D FileRecv "R:\Temp"
-MKLINK /D Image "R:\Temp"
-MKLINK /D Video "R:\Temp"
+MKLINK /D Audio %Ramdisk%
+MKLINK /D FileRecv %Ramdisk%
+MKLINK /D Image %Ramdisk%
+MKLINK /D Video %Ramdisk%
 IF %1 EQU "All Users" CD.. && EXIT /B
 RD /S /Q Ads 2>NUL
 RD /S /Q AppWebCache 2>NUL
 RD /S /Q OfflinePackage 2>NUL
-MKLINK /D Ads "R:\Temp"
-MKLINK /D AppWebCache "R:\Temp"
-MKLINK /D OfflinePackage "R:\Temp"
+MKLINK /D Ads %Ramdisk%
+MKLINK /D AppWebCache %Ramdisk%
+MKLINK /D OfflinePackage %Ramdisk%
 CD..
 EXIT /B
 :Exit
