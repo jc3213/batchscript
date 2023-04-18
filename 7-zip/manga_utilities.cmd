94eb406ed692203030251ab08f62d1450e9c08fc
jc3213 <jc3213@users.noreply.github.com>
2023-04-18 19:29:18 +0800

    fixed input null

--- 7-zip/manga_utilities.cmd
+++ 7-zip/manga_utilities.cmd
@@ -5,8 +5,6 @@ ECHO ==================================================================
 ECHO Remove original files or temporary files? (Y/y)
 ECHO ==================================================================
 SET /P Yes=^> 
-ECHO.
-ECHO.
 FOR %%I IN (%*) DO (CALL :Check %%I)
 GOTO :Exit
 :Check
@@ -15,13 +13,13 @@ IF %ErrorLevel% EQU 0 GOTO :NewPack
 :Repack
 ECHO a|"%Zip%" x %1 -o"%~DPN1"
 "%Zip%" a "%~DPN1.zip" "%~DPN1\*"
-IF /I %Yes% NEQ y EXIT /B
+IF /I [%Yes%] NEQ [y] EXIT /B
 RD /S /Q "%~DPN1"
 IF /I %~X1 NEQ .zip DEL %1 /S /Q
 EXIT /B
 :NewPack
 "%Zip%" a "%~DPNX1.zip" "*"
-IF /I %Yes% NEQ y EXIT /B
+IF /I [%Yes%] NEQ [y] EXIT /B
 CD..
 RD /S /Q "%~1"
 EXIT /B
