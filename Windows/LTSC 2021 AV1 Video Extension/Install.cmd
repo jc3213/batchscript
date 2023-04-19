b35e74ca83a84a122e718de70f201fe86291f783
jc3213 <jc3213@users.noreply.github.com>
2023-04-19 16:53:46 +0800

    Updated better readability

--- Windows/LTSC 2021 AV1 Video Extension/Install.cmd
+++ Windows/LTSC 2021 AV1 Video Extension/Install.cmd
@@ -1,5 +1,5 @@
-@ECHO OFF
-PUSHD %~DP0
-IF %PROCESSOR_ARCHITECTURE% EQU AMD64 (SET Arch=x64) ELSE (SET Arch=x86)
+@echo off
+pushd %~dp0
+if %Processor_Architecture% equ AMD64 (set Arch=x64) else (set Arch=x86)
 %SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Add-AppxPackage -Path 'Microsoft.AV1VideoExtension_1.1.52851.0_%Arch%__8wekyb3d8bbwe.Appx'"
-TIMEOUT /T 5
+timeout /t 5
