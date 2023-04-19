b9ac7ad49fc209427a9093e6daf48f9d5303e72e
jc3213 <jc3213@users.noreply.github.com>
2023-04-19 22:23:09 +0800

    oops

--- ffmpeg/ffmpeg_remux_video.cmd
+++ ffmpeg/ffmpeg_remux_video.cmd
@@ -1,20 +1,18 @@
 @echo off
 pushd %~dp0bin
 for %%a in (%*) do (call :Check %%a)
-echo %video% %audio%
-pause
 if not defined video goto :Exit
 if not defined audio goto :Exit
-ffmpeg.exe -fflags +genpts %Video% %Audio% -c:v copy -c:a copy %Output%
+ffmpeg.exe -fflags +genpts %video% %audio% -c:v copy -c:a copy %output%
 :Exit
 timeout /t 5
 exit
 :Check
 for /f "tokens=3,4 delims= " %%a in ('ffmpeg.exe -i %1 2^>^&1 ^| findstr /i "video audio"') do (
 	if "%%a"=="Audio:" (
-        set Audio=-i %1
+        set audio=-i %1
     ) else if "%%a"=="Video:" (
-        set Video=-i %1
-        set Output="%~dp1remuxed_%~n1.mkv"
+        set video=-i %1
+        set output="%~dp1remuxed_%~n1.mkv"
     )
 )
