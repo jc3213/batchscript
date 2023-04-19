335bf14d107008afa3862aa9647c65f1cf464e98
jc3213 <jc3213@users.noreply.github.com>
2023-04-19 22:58:42 +0800

    Updated better readability

--- ffmpeg/ffmpeg_cut_clips.cmd
+++ ffmpeg/ffmpeg_cut_clips.cmd
@@ -1,39 +1,35 @@
 @echo off
 pushd %~dp0bin
 set file=%1
-call :Separator
-echo File path sample: C:\Program Files\ffmpeg\my video.mp4
-echo Split after specific timestamp: 00:01:02.345
-echo Split by parts based on timestamp: 00:01:02.123-00:03:04.345
-echo Short timestamp format 01:23.456 and 12.345 are also available
-call :Separator
-echo.
+if defined file goto :Main
+timeout /t 5
+exit
 :Main
-call :File
-call :Time
-if not defined to (
-    call :Stamp %file%
-) else (
-    call :Period %file%
-)
-call :Clear
-goto :Main
-:File
-if not defined file (
-    set /p file=Select File: 
-) else (
-    echo Current file: %file%
-)
-if not defined file goto :File
-exit /b
+echo =======================================================================
+echo Split video by timestamp: 00:01:23.345
+echo Cut video during periods: 00:04:55.123-00:08:32.690
+echo File: %1
+echo =======================================================================
 :Time
-set /p clip=Timestamp: 
+set /p clip=^> 
+echo.
 if not defined clip goto :Time
 for /f "tokens=1-2 delims=-" %%a in ('echo %clip%') do (
     set ss=%%a
 	set to=%%b
 )
-exit /b
+if not defined to (goto :Stamp) else (goto :Period)
+:Clear
+set clip=
+set name=
+set stamp=
+set ss=
+set to=
+echo.
+echo.
+echo.
+echo.
+goto :Main
 :Name
 for /f "tokens=1-3 delims=:" %%a in ('echo %1') do (
     set tf1=%%a
@@ -61,13 +57,15 @@ set out1=%~dp1%~n1_%fn0%_1%~x1
 set out2=%~dp1%~n1_%fn0%_2%~x1
 ffmpeg.exe -i "%1" -to %ss% -c copy %out1%
 ffmpeg.exe -i "%1" -ss %ss% -c copy %out2%
-call :Warning WarnStamp
-exit /b
-:WarnStamp
-echo [Mode]        Split after specific timestamp
+echo.
+echo.
+echo =======================================================================
+echo [Mode]        Split video by timestamp
 echo [Timestamp]   %ts0%
-echo [Output]      %out1%, %out2%
-exit /b
+echo [Output]      %out1%
+echo [Output]      %out2%
+echo =======================================================================
+goto :Clear
 :Period
 call :Name %ss%
 set fn1=%name%
@@ -77,28 +75,12 @@ set fn2=%name%
 set ts2=%stamp%
 set out0=%~dp1%~n1_%fn1%-%fn2%%~x1
 ffmpeg.exe -i "%1" -ss %ss% -to %to% -c copy %out0%
-call :Warning WarnPeriod
-exit /b
-:WarnPeriod
-echo [Mode]        Split by parts based on timestamp
-echo [Timestamp]   %ts1%-%ts2%
-echo [Output]      %out0%
-exit /b
-:Warning
 echo.
-call :Separator
-echo [input]       %file%
-call :%1
-call :Separator
 echo.
-exit /b
-:Separator
 echo =======================================================================
-exit /b
-:Clear
-set clip=
-set name=
-set stamp=
-set ss=
-set to=
-exit /b
+echo [Mode]        Cut video during periods
+echo [Period]      %ts1%-%ts2%
+echo [Output]      %out0%
+echo =======================================================================
+echo.
+goto :Clear
