a0414d8bf96ec78e91fdba1798635952398d6750
jc3213 <jc3213@users.noreply.github.com>
2023-04-19 23:18:25 +0800

    oops

--- aria2c/aria2c.cmd
+++ aria2c/aria2c.cmd
@@ -1,32 +1,26 @@
 @echo off
 pushd %~dp0
-if [%1] equ [/h] goto :Hide
+if [%1] equ [/h] goto :NoWindow
 if [%1] equ [/r] goto :Register
 if [%1] equ [/u] goto :Unregister
-:App
-if not exist aria2c.session call :Session
+if not exist aria2c.session type nul > aria2c.session
 bin\aria2c.exe --conf=aria2c.conf
-exit
-:Hide
-if not exist aria2c.session call :Session
-if not exist aria2c.vbs call :Startup
-aria2c.vbs
-exit
 :Register
 reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /t "REG_SZ" /d "%CD%\aria2c.vbs" /f
-goto :Hide
-:Unregister
-taskkill /im "aria2c.exe"
-reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /f
-timeout /t 5
+:NoWindow
+if not exist aria2c.session type nul > aria2c.session
+if not exist aria2c.vbs call :VBS
+aria2c.vbs
 exit
-:Session
-type nul>aria2c.session
-exit /b
-:Startup
+:VBS
 echo Set objSh = CreateObject("WScript.Shell")>aria2c.vbs
 echo Set objFSO = CreateObject("Scripting.FileSystemObject")>>aria2c.vbs
 echo Set objFile = objFSO.GetFile(Wscript.ScriptFullName)>>aria2c.vbs
 echo objDir = objFSO.GetParentFolderName(objFile)>>aria2c.vbs
 echo objSh.run "%WinDir%\System32\cmd.exe /k pushd " ^& objDir ^& " && bin\aria2c.exe --conf=aria2c.conf", ^0>>aria2c.vbs
 exit /b
+:Unregister
+taskkill /im "aria2c.exe"
+reg delete "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /f
+timeout /t 5
+
