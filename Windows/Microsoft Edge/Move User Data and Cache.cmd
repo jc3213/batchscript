6762e40f249b2898a4faee42c16810e8dca140ff
jc3213 <jc3213@users.noreply.github.com>
2023-04-19 16:51:59 +0800

    Delete Move User Data and Cache.cmd

--- Windows/Microsoft Edge/Move User Data and Cache.cmd
+++
@@ -1,5 +0,0 @@
-@ECHO OFF
-FOR /F %%I in ('wmic logicaldisk where "VolumeName='RAMDISK'" get Caption ^| find ":"') DO SET Ramdisk=%%I\Temp
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Edge" /V "UserDataDir"  /T "REG_SZ" /D "%UserProfile%\Documents\EdgeUserData" /F
-REG ADD "HKLM\SOFTWARE\Policies\Microsoft\Edge" /V "DiskCacheDir" /T "REG_SZ" /D "%Ramdisk%" /F
-TIMEOUT /T 5
