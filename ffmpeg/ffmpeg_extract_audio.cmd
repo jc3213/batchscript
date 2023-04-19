98aa390bdee73183332074621eedfc4c1c22d183
jc3213 <jc3213@users.noreply.github.com>
2023-04-19 21:52:09 +0800

    Updated better readability

--- ffmpeg/ffmpeg_extract_audio.cmd
+++ ffmpeg/ffmpeg_extract_audio.cmd
@@ -5,23 +5,23 @@ for %%a in (%*) do (call :Audio %%a)
 timeout /t 5
 exit
 :Audio
-for /f "tokens=3,4 delims= " %%a in ('ffmpeg.exe -i %1 2^>^&1 ^| findstr /i "audio"') do (if %%a equ Audio: set Codec=%%b)
-if [%Codec%] equ [aac] set Format=m4a
-if [%Codec%] equ [opus] set Format=webm
-if [%Codec%] equ [mp3] set Format=mp3
-if [%Codec%] equ [vorbis] set Format=ogg
-if [%Codec%] equ [pcm_s16le] set Format=wav
-if [%Codec%] equ [flac] set Format=flac
-if [%Codec%] equ [alac] set Format=m4a
-if defined Format goto :Extract
+for /f "tokens=3,4 delims= " %%a in ('ffmpeg.exe -i %1 2^>^&1 ^| findstr /i "audio"') do (if %%a equ Audio: set codec=%%b)
+if [%codec%] equ [aac] set ext=m4a
+if [%codec%] equ [opus] set ext=webm
+if [%codec%] equ [mp3] set ext=mp3
+if [%codec%] equ [vorbis] set ext=ogg
+if [%codec%] equ [pcm_s16le] set ext=wav
+if [%codec%] equ [flac] set ext=flac
+if [%codec%] equ [alac] set ext=m4a
+if defined ext goto :Extract
 echo ==========================================================================
-echo Unknown audio codec %Codec% of %~nx1
+echo Unknown audio codec %codec% of %~nx1
 echo Please set the file extention
 echo ==========================================================================
 :Format
-set /p Format=^> 
+set /p ext=^> 
 echo.
-if not defined Format goto :Format
+if not defined ext goto :Format
 :Extract
-ffmpeg.exe -i %1 -vn -acodec copy %~dpn1.%Format%
+ffmpeg.exe -i %1 -vn -acodec copy %~dpn1.%ext%
 exit /b
--- ffmpeg/ffmpeg_merge_tracks.cmd
+++ ffmpeg/ffmpeg_merge_tracks.cmd
@@ -1,14 +1,13 @@
 @echo off
 pushd %~dp0bin
+if not exist %1 goto :Exit
 set /a index=0
-set File=%~x1
-:Files
-if not exist "%~1" goto :Proc
-set input=-i "%1" %input%
-set /a index+=1
-for /f "tokens=1,* delims= " %%a in ("%*") do call :Files %%b
-:Proc
-if not defined input goto :End
-ffmpeg.exe %input% -filter_complex "amerge=inputs=%index%" merged_audio_track%File%
-:End
+for %%a in (%*) do (call :Merge %%a)
+ffmpeg.exe %input% -filter_complex "amerge=inputs=%index%" %~dp1merged_audio_track%~x1
+:Exit
 timeout /t 5
+exit
+:Merge
+set /a index+=1
+set input=-i "%1" %input%
+exit /b
