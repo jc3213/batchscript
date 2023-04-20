3b934ed8b92040eec07df9a43cb28334f29e44f3
jc3213 <jc3213@users.noreply.github.com>
2023-04-20 14:16:05 +0800

    oops

--- misc/batch_rename_files.cmd
+++ misc/batch_rename_files.cmd
@@ -2,51 +2,50 @@
 if not exist %1 exit
 cd /d %1
 setlocal enabledelayedexpansion
-set /a Seek=1
-:SeekFiles
-if [%Index%] equ [] set Index=1-9
-for /f "tokens=*" %%a in ('dir /b /a-d') do (
-    for /f "tokens=%Index% delims=-_.() " %%b in ("%%a") do (
-        if %Seek% equ 1 (
-            echo Option		  		%%a
-            echo ===========================================================
-            call :ListIndex %%b %%c %%d %%e %%f %%g %%h %%i %%j && goto :eof
-        ) else if %Seek% gtr 1 (
-            call :Filename "%%a" "%%b"
-        )
-    )
-)
+set /a seek=1
+for /f %%a in ('dir /b /a-d') do (call :Menu "%%a")
+:Menu
+echo ===========================================================
+:Option
+set filename=%~1
+if not defined filename goto :MenuEnd
+for /f "tokens=1* delims=-_.() " %%a in ("%filename%") do (call :Text "%%a" "%%b")
+:MenuEnd
+echo ===========================================================
+:Input
+set /p index=^> 
+echo.
+if not defined index goto :Input
+echo %index%| findstr /r /c:"^[0-9][0-9]*$" >nul || goto :Input
+for /f %%a in ('dir /b /a-d') do (call :Rename "%%a")
 timeout /t 5
 exit
-:ListIndex
-if "%1"=="" goto :SeekIndex
-echo %1| findstr /r /c:"^[0-9][0-9]*$" >nul && set Symbol=** || set Symbol=
-echo %Seek%		^>^>		%~1	  %Symbol%
-set /a Seek+=1
-for /f "tokens=1,* delims= " %%a in ("%*") do (call :ListIndex %%b)
-:SeekIndex
-set /p Index=Which one?  
-if %Index% LSS 1 goto :SeekIndex
-if %Index% GEQ %Seek% goto :SeekIndex 
-goto :SeekFiles
+:Text
+echo %~1| findstr /r /c:"^[0-9][0-9]*$" >nul && set better=** || set better=
+echo %seek%        ^>^>        %~1  %better%
+set /a seek+=1
+call :Option %2
+:Rename
+for /f "tokens=1 delims=-_.() " %%a in (%1) do (call :Filename %1 "%%a")
+exit /b
 :Filename
-set String=#%~2
-set Length=0
+set string=#%~2
+set length=0
 for %%k in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
-    if "!String:~%%k,1!" neq "" (
-        set /a Length+=%%k
-        set String=!String:~%%k!
+    if "!string:~%%k,1!" neq "" (
+        set /a length+=%%k
+        set string=!string:~%%k!
     )
 )
-if %Length% equ 1 (
-    set Prefix=000%~2
-) else if %Length% equ 2 (
-    set Prefix=00%~2
-) else if %Length% equ 3 (
-    set Prefix=0%~2
+if %length% equ 1 (
+    set name=000%~2
+) else if %length% equ 2 (
+    set name=00%~2
+) else if %length% equ 3 (
+    set name=0%~2
 ) else (
-    set Prefix=%~2
+    set name=%~2
 )
-echo %~1        %~2        %Prefix%%~X1
-ren %1 %Prefix%%~X1
+echo %~1        %~2        %name%%~X1
+ren %1 %name%%~X1
 exit /b
