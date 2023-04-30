635e56b0babd5f29d6d596671fdf3a0e7ccc76e8
jc3213 <jc3213@users.noreply.github.com>
2023-04-30 21:54:17 +0800

    add windows defender

--- Microsoft/Microsoft Windows Tweaks.cmd
+++ Microsoft/Microsoft Windows Tweaks.cmd
@@ -3,16 +3,18 @@ set backup=%~dp0microcode_backup.zip
 :main
 title Microsoft Windows Tweaks
 echo ==================================================================
-echo 1. Manage Power plan
-echo 2. Manage Diagnostic
-echo 3. Manage Maintenance
-echo 4. Manage CPU microcode
+echo 1. Manage power plan
+echo 2. Manage diagnostic
+echo 3. Manage maintenance
+echo 4. Manage anti-virus
+echo 5. Manage CPU microcode
 echo ==================================================================
 set /p main=^> 
 if [%main%] equ [1] goto :powerp
 if [%main%] equ [2] goto :wndiag
 if [%main%] equ [3] goto :wnmain
-if [%main%] equ [4] goto :cpumic
+if [%main%] equ [4] goto :antivr
+if [%main%] equ [5] goto :cpumic
 goto :back
 :powerp
 echo.
@@ -128,6 +130,79 @@ goto :clear
 echo.
 sc config "SysMain" start=auto
 sc start "SysMain"
+goto :clear
+:antivr
+echo.
+title Manage Microsoft Defender
+echo ==================================================================
+echo 1. Disable context menu
+echo 2. Restore context menu
+echo 3. Disable system tray icon
+echo 4. Restore system tray icon
+echo 5. Disable scheduled scan
+echo 6. Restore scheduled scan
+echo 7. Disable real-time protection
+echo 8. Restore real-time protection
+echo 0. Back to main menu
+echo ==================================================================
+set /p avr=^> 
+if [%avr%] equ [1] goto :ctxoff
+if [%avr%] equ [2] goto :ctxmon
+if [%avr%] equ [3] goto :traoff
+if [%avr%] equ [4] goto :trayon
+if [%avr%] equ [5] goto :scnoff
+if [%avr%] equ [6] goto :scanon
+if [%avr%] equ [7] goto :reloff
+if [%avr%] equ [8] goto :realon
+if [%avr%] equ [0] goto :back
+goto :antivr
+:ctxoff
+echo.
+reg delete "HKCR\*\shellex\ContextMenuHandlers\EPP" /ve /f
+reg delete "HKCR\Drive\shellex\ContextMenuHandlers\EPP" /ve /f
+reg delete "HKCR\Directory\shellex\ContextMenuHandlers\EPP" /ve /f
+reg delete "HKCR\CLSID\{09A47860-11B0-4DA5-AFA5-26D86198A780}\InprocServer32" /ve /f
+goto :clear
+:ctxmon
+echo.
+reg add "HKCR\*\shellex\ContextMenuHandlers\EPP" /ve /t "REG_SZ" /d "{09A47860-11B0-4DA5-AFA5-26D86198A780}" /f
+reg add "HKCR\Drive\shellex\ContextMenuHandlers\EPP" /ve /t "REG_SZ" /d "{09A47860-11B0-4DA5-AFA5-26D86198A780}" /f
+reg add "HKCR\Directory\shellex\ContextMenuHandlers\EPP" /ve /t "REG_SZ" /d "{09A47860-11B0-4DA5-AFA5-26D86198A780}" /f
+reg add "HKCR\CLSID\{09A47860-11B0-4DA5-AFA5-26D86198A780}\InprocServer32" /ve /t "REG_SZ" /d "%ProgramFiles%\Windows Defender\shellext.dll" /f
+goto :clear
+:traoff
+echo.
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Systray" /v "HideSystray" /t "REG_DWORD" /D "0x00000001" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /v "SecurityHealth" /t "REG_BINARY" /D "07000000CD54F699D161D900" /f
+reg delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "SecurityHealth" /f
+goto :clear
+:trayon
+echo.
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender Security Center\Systray" /v "HideSystray" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\StartupApproved\Run" /v "SecurityHealth" /t "REG_BINARY" /d "060000000000000000000000" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "SecurityHealth" /t "REG_EXPAND_SZ" /d "%%windir%%\system32\SecurityHealthSystray.exe" /f
+goto :clear
+:scnoff
+echo.
+schtasks /change /disable /tn "\Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan"
+goto :clear
+:scanon
+echo.
+schtasks /change /enable /tn "\Microsoft\Windows\Windows Defender\Windows Defender Scheduled Scan"
+goto :clear
+:reloff
+echo.
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /t "REG_DWORD" /D "0x00000001" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableRealtimeMonitoring" /t "REG_DWORD" /D "0x00000001" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableBehaviorMonitoring" /t "REG_DWORD" /D "0x00000001" /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v "DisableScanOnRealtimeEnable" /t "REG_DWORD" /D "0x00000001" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WinDefend" /v "Start" /t "REG_DWORD" /D "0x00000004" /f
+goto :clear
+:realon
+echo.
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender" /v "DisableAntiSpyware" /f
+reg delete "HKLM\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /f
+reg add "HKLM\SYSTEM\CurrentControlSet\Services\WinDefend" /v "Start" /t "REG_DWORD" /d "0x00000002" /f
 goto :clear
 :cpumic
 echo.
