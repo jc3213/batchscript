fcc8b9c6c0c86613509543489236332f9101acd3
jc3213 <jc3213@users.noreply.github.com>
2023-04-30 10:39:40 +0800

    Update run_with_administrative_privileges.cmd

--- misc/run_with_administrative_privileges.cmd
+++ misc/run_with_administrative_privileges.cmd
@@ -1,7 +1,7 @@
 @echo off
 net session >nul 2>&1
-if %errorlevel% == 0 goto :admin
-start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c pushd ""%~dp0"" && ""%~s0"" ::","","runas",1)(window.close)
+if %ErrorLevel% == 0 goto :admin
+start "" mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c pushd ""%~dp0"" && ""%~s0","","runas",1)(window.close)
 exit
 :admin
 echo Hello, world!
