a44fee7e7bc9d8b55635457b8c1d50a6a7e9aa9d
jc3213 <jc3213@users.noreply.github.com>
2023-05-01 07:42:34 +0800

    Update Add Context Menu.cmd

--- Windows/Grant administrator privileges/Add Context Menu.cmd
+++ Windows/Grant administrator privileges/Add Context Menu.cmd
@@ -1,21 +1,13 @@
 @echo off
-set File=HKCR\*\shell\runas
-set Folder=HKCR\Directory\shell\runas
-set Menu=Grant administrator privileges
-set Icon=C:\Windows\system32\imageres.dll,-78
-set Admin=%%SystemRoot%%\system32\cmd.exe /c takeown /f \"%%1\" ^&^& icacls \"%%1\" /grant Administrators:F
-set Admin@=%%SystemRoot%%\system32\cmd.exe /c takeown /f \"%%1\" /r /d y ^&^& icacls \"%%1\" /grant Administrators:F /t
-::File
-reg add "%File%" /ve /t "REG_SZ" /d "%Menu%" /f
-reg add "%File%" /v "Icon" /t "REG_SZ" /d "%Icon%" /f
-reg add "%File%" /v "NoWorkingDirectory"  /t "REG_SZ" /d "" /f
-reg add "%File%\command" /ve /t "REG_EXPAND_SZ" /d "%Admin%" /f
-reg add "%File%\command" /v "IsolatedCommand" /t "REG_EXPAND_SZ" /d "%Admin%" /f
-::Folder
-reg add "%Folder%" /ve /t "REG_SZ" /d "%Menu%" /f
-reg add "%Folder%" /v "Icon" /t "REG_SZ" /d "%Icon%" /f
-reg add "%Folder%" /v "NoWorkingDirectory" /t "REG_SZ" /d "" /f
-reg add "%Folder%\command" /ve /t "REG_EXPAND_SZ" /d "%Admin@%" /f
-reg add "%Folder%\command" /v "IsolatedCommand" /t "REG_EXPAND_SZ" /d "%Admin@%" /f
-::End
+call :registry "HKCR\*\shell\runas"
+call :registry "HKCR\Directory\shell\runas" "/r /d y"
+goto :back
+:registry
+reg add "%~1" /ve /t "REG_SZ" /d "Grant administrator privileges" /f
+reg add "%~1" /v "Icon" /t "REG_SZ" /d "C:\Windows\system32\imageres.dll,-78" /f
+reg add "%~1" /v "NoWorkingDirectory"  /t "REG_SZ" /d "" /f
+reg add "%~1\command" /ve /t "REG_EXPAND_SZ" /d "%%SystemRoot%%\system32\cmd.exe /c takeown /f \"%%1\" %~2 && icacls \"%%1\" /grant Administrators:F" /f
+reg add "%~1\command" /v "IsolatedCommand" /t "REG_EXPAND_SZ" /d "%%SystemRoot%%\system32\cmd.exe /c takeown /f \"%%1\" %~2 && icacls \"%%1\" /grant Administrators:F" /f
+exit /b
+:back
 timeout /t 5
