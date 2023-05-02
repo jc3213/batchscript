c88e4f6467b5b24c125e4ead856871dc58e0e0d7
jc3213 <jc3213@users.noreply.github.com>
2023-05-02 08:54:54 +0800

    Update batch_rename_files.cmd

--- misc/batch_rename_files.cmd
+++ misc/batch_rename_files.cmd
@@ -1,50 +1,35 @@
 @echo off
 if not exist %1 exit
 cd /d %1
-setlocal enabledelayedexpansion
 set /a seek=1
-for /f %%a in ('dir /b /a-d') do (call :Menu "%%a")
-:Menu
+for /f %%a in ('dir /b /a-d') do (call :main "%%a")
+:main
 echo ===========================================================
-:Option
+:menst
 set filename=%~1
-if not defined filename goto :MenuEnd
-for /f "tokens=1* delims=-_.() " %%a in ("%filename%") do (call :Text "%%a" "%%b")
-:MenuEnd
+if not defined filename goto :mened
+for /f "tokens=1* delims=-_.() " %%a in ("%filename%") do (call :menu "%%a" "%%b")
+:mened
 echo ===========================================================
-:Input
+:input
 set /p index=^> 
 echo.
-echo %index%| findstr /r "^[1-9][0-9]*$" >nul || goto :Input
-for /f %%a in ('dir /b /a-d') do (call :Rename "%%a")
+echo %index%| findstr /r "^[1-9][0-9]*$" >nul || goto :input
+for /f %%a in ('dir /b /a-d') do (call :rename "%%a")
 timeout /t 5
 exit
-:Text
+:menu
 echo %~1| findstr /r "^[0-9][0-9]*$" >nul && set better=** || set better=
 echo %seek%            ^>^>            %~1        %better%
 set /a seek+=1
-call :Option %2
-:Rename
-for /f "tokens=%index% delims=-_.() " %%a in (%1) do (call :Filename %1 "%%a")
+call :menst %2
+:rename
+for /f "tokens=%index% delims=-_.() " %%a in (%1) do (call :exec %1 "%%a")
 exit /b
-:Filename
-set string=#%~2
-set length=0
-for %%k in (4096 2048 1024 512 256 128 64 32 16 8 4 2 1) do (
-    if "!string:~%%k,1!" neq "" (
-        set /a length+=%%k
-        set string=!string:~%%k!
-    )
-)
-if %length% equ 1 (
-    set name=000%~2
-) else if %length% equ 2 (
-    set name=00%~2
-) else if %length% equ 3 (
-    set name=0%~2
-) else (
-    set name=%~2
-)
+:exec
+set input=%~2
+set name=000%input%
+set name=%name:~-4%
 echo %~1        %~2        %name%%~X1
 ren %1 %name%%~X1
 exit /b
