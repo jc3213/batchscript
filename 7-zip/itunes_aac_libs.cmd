f3954082c6478896a5a6cabc53d3aaf3eaad45f5
jc3213 <jc3213@users.noreply.github.com>
2023-05-03 14:45:56 +0800

    Update itunes_aac_libs.cmd

--- 7-zip/itunes_aac_libs.cmd
+++ 7-zip/itunes_aac_libs.cmd
@@ -1,10 +1,19 @@
 @echo off
 pushd %~dp0
 for /f "tokens=2*" %%a in ('reg query "HKLM\Software\7-Zip" /v "Path"') do (set zip=%%b7z.exe)
-if %Processor_architecture% equ AMD64 (call :x64) else (call :x86)
-set setup=iTunes%arch%Setup.exe
+if %Processor_architecture% equ AMD64 goto :x64
+set bit=32
+goto :main
+:x64
+set bit=64
+set ver=64
+:main
+set setup=iTunes%bit%Setup.exe
+set output=iTunes%ver%
+set iTunes=iTunes%ver%.msi
+set unpack=%CD%\_iTunes%ver%
 if exist %setup% goto :unzip
-curl https://www.apple.com/itunes/download/win%arch% --location --output %setup%
+curl https://www.apple.com/itunes/download/win%bit% --location --output %setup%
 :unzip
 "%zip%" e -y %setup% %iTunes%
 md %unpack% 2>nul
@@ -22,16 +31,3 @@ del %iTunes% 2>nul
 del %setup% 2>nul
 rd %unpack% /s /q 2>nul
 timeout /t 5
-exit
-:x64
-set arch=64
-set output=iTunes64
-set iTunes=iTunes64.msi
-set unpack=%CD%\_iTunes64
-exit /b
-:x86
-set arch=32
-set output=iTunes
-set iTunes=iTunes.msi
-set unpack=%CD%\_iTunes
-exit /b
