3533c0193e4f5155e65c69b852177c3466eba5b3
jc3213 <jc3213@users.noreply.github.com>
2023-05-03 10:20:15 +0800

    add check and download foobar2000

--- 7-zip/foobar2000_portable.cmd
+++ 7-zip/foobar2000_portable.cmd
@@ -1,11 +1,44 @@
 @echo off
 pushd %~dp0
-for /f "tokens=2*" %%a in ('reg query "HKLM\Software\7-Zip" /v "Path"') do (set zip=%%b7z.exe)
-"%zip%" x %1 -y -x!$PLUGINSDIR -x!$R0 -x!uninstall.exe -o"%~dnp1"
-type nul > "%~dnp1\portable_mode_enabled"
+set /a int=0
+for /f "tokens=*" %%a in ('curl https://www.foobar2000.org/download --silent ^| findstr /r /c:"/getfile/foobar2000_.*\.exe"') do (call :menu "%%a")
+:menu
+setlocal enabledelayedexpansion
+echo ====================================================================================================
+for /f "tokens=2,5,8 delims=>=" %%a in (%1) do (call :link %%a %%b %%c)
+echo ====================================================================================================
+:act
+set /p act=^> 
+set url=!link[%act%]!
+set app=!name[%act%]!
+setlocal disabledelayedexpansion
+if not defined url goto :act
+if exist %app%.exe goto :unpack
 echo.
 echo.
-echo Portable location %~dnp1
+echo Downloading: %url%
+curl %url% --location --silent --output %app%.exe
+:unpack
+echo.
+echo.
+echo Make Portable: %app%
+for /f "tokens=1,2*" %%a in ('reg query HKLM\Software\7-Zip /V Path') do (if "%%a"=="Path" set Zip=%%c7z.exe)
+"%Zip%" x "%~dp0%app%.exe" -y -x!$PLUGINSDIR -x!$R0 -x!uninstall.exe -o"%~dp0%app%"
+type nul > %app%\portable_mode_enabled
+echo.
+echo.
+echo Portable location foobar2000
 echo.
 echo.
 timeout /t 5
+exit
+:link
+for %%a in (%*) do (call :trim %%a)
+exit /b
+:trim
+set /a int+=1
+set link=%~1
+set link[%int%]=https://www.foobar2000.org%link:getfile/=files/%
+set name[%int%]=%link:~9%
+echo %int%. !link[%int%]!
+exit /b
