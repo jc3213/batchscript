5b719c27e49194c90a6f8a537d060209ce9ec5af
jc3213 <jc3213@users.noreply.github.com>
2023-05-03 11:05:25 +0800

    fixed logic, delete temp exe file

--- 7-zip/foobar2000_portable.cmd
+++ 7-zip/foobar2000_portable.cmd
@@ -1,6 +1,7 @@
 @echo off
 pushd %~dp0
 set /a int=0
+for /f "tokens=2*" %%a in ('reg query "HKLM\Software\7-Zip" /v "Path"') do (set zip=%%b7z.exe)
 for /f "tokens=*" %%a in ('curl https://www.foobar2000.org/download --silent ^| findstr /r /c:"/getfile/foobar2000_.*\.exe"') do (call :menu "%%a")
 :menu
 setlocal enabledelayedexpansion
@@ -21,13 +22,15 @@ curl %url% --location --silent --output %app%.exe
 :unpack
 echo.
 echo.
-echo Make Portable: %app%
-for /f "tokens=1,2*" %%a in ('reg query HKLM\Software\7-Zip /V Path') do (if "%%a"=="Path" set Zip=%%c7z.exe)
-"%Zip%" x "%~dp0%app%.exe" -y -x!$PLUGINSDIR -x!$R0 -x!uninstall.exe -o"%~dp0%app%"
+echo Make Portable: %app%.exe
+"%zip%" x "%~dp0%app%.exe" -y -x!$PLUGINSDIR -x!$R0 -x!uninstall.exe -o"%~dp0%app%"
 type nul > %app%\portable_mode_enabled
 echo.
 echo.
-echo Portable location foobar2000
+del /s /q %app%.exe
+echo.
+echo.
+echo Portable Location: %~dp0%app%
 echo.
 echo.
 timeout /t 5
@@ -37,8 +40,8 @@ for %%a in (%*) do (call :trim %%a)
 exit /b
 :trim
 set /a int+=1
-set link=%~1
-set link[%int%]=https://www.foobar2000.org%link:getfile/=files/%
-set name[%int%]=%link:~9%
+set temp=%~1
+set link[%int%]=https://www.foobar2000.org%temp:getfile=files%
+set name[%int%]=%temp:~9,-4%
 echo %int%. !link[%int%]!
 exit /b
