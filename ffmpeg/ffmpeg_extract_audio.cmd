fd29bf931bb1e6d228611d34d3112bf79da89880
jc3213 <jc3213@users.noreply.github.com>
2023-05-04 09:24:56 +0800

    Update ffmpeg_extract_audio.cmd

--- ffmpeg/ffmpeg_extract_audio.cmd
+++ ffmpeg/ffmpeg_extract_audio.cmd
@@ -1,9 +1,9 @@
 @echo off
 pushd %~dp0bin
-for %%a in (%*) do (call :Audio %%a)
+for %%a in (%*) do (call :codec %%a)
 timeout /t 5
 exit
-:Audio
+:codec
 for /f "tokens=3,4 delims= " %%a in ('ffmpeg.exe -i %1 2^>^&1 ^| findstr /i "audio"') do (if %%a equ Audio: set codec=%%b)
 if [%codec%] equ [aac] set ext=m4a
 if [%codec%] equ [opus] set ext=webm
@@ -12,14 +12,16 @@ if [%codec%] equ [vorbis] set ext=ogg
 if [%codec%] equ [pcm_s16le] set ext=wav
 if [%codec%] equ [flac] set ext=flac
 if [%codec%] equ [alac] set ext=m4a
-if defined ext goto :Extract
+if defined ext goto :extract
+echo Unknown codec detected
 echo ==========================================================================
-echo Unknown audio codec %codec% of %~nx1
-echo Please set the file extention
+echo File name: %~nx1
+echo Audio codec: %codec%
+echo Please set file extension
 echo ==========================================================================
-:Format
+:format
 set /p ext=^> 
 echo.
-if not defined ext goto :Format
-:Extract
+if not defined ext goto :format
+:extract
 ffmpeg.exe -i %1 -vn -acodec copy %~dpn1.%ext%
