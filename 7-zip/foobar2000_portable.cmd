6075ee538dd6dc5a9386271a6f67a860cc686d34
jc3213 <jc3213@users.noreply.github.com>
2023-05-04 23:32:28 +0800

    Update foobar2000_portable.cmd

--- 7-zip/foobar2000_portable.cmd
+++ 7-zip/foobar2000_portable.cmd
@@ -1,29 +1,15 @@
 @echo off
 pushd %~dp0
 for /f "tokens=2*" %%a in ('reg query "HKLM\Software\7-Zip" /v "Path"') do (set zip=%%b7z.exe)
-for /f "tokens=*" %%a in ('curl https://www.foobar2000.org/download --silent ^| findstr /r /c:"/getfile/foobar2000_.*\.exe"') do (call :menu "%%a")
-:menu
-setlocal enabledelayedexpansion
-echo ====================================================================================================
-for /f "tokens=2,5,8 delims=>=" %%a in (%1) do (call :link %%a %%b %%c)
-echo ====================================================================================================
-:act
-set /p act=^> 
-set url=!link[%act%]!
-set app=!name[%act%]!
-setlocal disabledelayedexpansion
-if not defined url goto :act
-if exist %app%.exe goto :unpack
-echo.
-echo.
-echo Downloading: %url%
-echo.
-echo.
+for /f "tokens=3 delims= " %%a in ('curl https://www.foobar2000.org/download --silent ^| find "Download foobar2000 v"') do (set ver=%%a)
+if %Processor_Architecture% equ AMD64 set arc=-x64
+if %Processor_Architecture% equ ARM64 set arc=-arm64ec
+set app=foobar2000%arc%_%ver%.exe
+set url=https://www.foobar2000.org/files/%app%
 curl %url% --location --output %app%.exe
-:unpack
 echo.
 echo.
-echo Make portable: %app%.exe
+echo Make portable - %~dp0%app%
 "%zip%" x "%~dp0%app%.exe" -y -x!$PLUGINSDIR -x!$R0 -x!uninstall.exe -o"%~dp0%app%"
 type nul > %app%\portable_mode_enabled
 echo.
@@ -31,18 +17,5 @@ echo.
 del /s /q %app%.exe
 echo.
 echo.
-echo Portable location: %~dp0%app%
-echo.
-echo.
 timeout /t 5
 exit
-:link
-for %%a in (%*) do (call :trim %%a)
-exit /b
-:trim
-set /a int+=1
-set temp=%~1
-set link[%int%]=https://www.foobar2000.org%temp:getfile=files%
-set name[%int%]=%temp:~9,-4%
-echo %int%. !link[%int%]!
-exit /b
