3faa15913d62e57bd4839787ba4e6ac656a52842
jc3213 <jc3213@users.noreply.github.com>
2023-05-04 23:19:35 +0800

    Update 7-zip_installer.cmd

--- 7-zip/7-zip_installer.cmd
+++ 7-zip/7-zip_installer.cmd
@@ -1,21 +1,19 @@
 @echo off
 pushd %~dp0
 title 7-Zip Installer
-for /f "tokens=3 delims= " %%a in ('curl https://7-zip.org/ --silent ^| findstr /r /c:"Download 7-Zip"') do (set ver=%%a)
+for /f "tokens=3 delims= " %%a in ('curl https://7-zip.org/ --silent ^| findstr /c:"Download 7-Zip"') do (set ver=%%a)
 if %Processor_Architecture% equ AMD64 set arc=-x64
 if %Processor_Architecture% equ ARM64 set arc=-arm64
 set app=7z%ver:.=%%arc%.exe
 set url=https://7-zip.org/a/%app%
-if exist %app% goto :install
 curl %url% --location --output %app%
 echo.
 echo.
-:install
 echo Install - 7-Zip %ver%
-echo %app% /S /D="%ProgramFiles%\7-Zip"
+%app% /S /D="%ProgramFiles%\7-Zip"
 echo.
 echo.
-echo del /s /q %app%
+del /s /q %app%
 echo.
 echo.
 echo Done!
