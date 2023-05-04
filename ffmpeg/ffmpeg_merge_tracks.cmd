ca12a0352201da1d3d424bf6516ad4f84ada639a
jc3213 <jc3213@users.noreply.github.com>
2023-05-04 09:26:50 +0800

    Update ffmpeg_merge_tracks.cmd

--- ffmpeg/ffmpeg_merge_tracks.cmd
+++ ffmpeg/ffmpeg_merge_tracks.cmd
@@ -1,12 +1,11 @@
 @echo off
 pushd %~dp0bin
-set /a index=0
-for %%a in (%*) do (call :Merge %%a)
-if %index% lss 2 goto :Exit
+for %%a in (%*) do (call :merge %%a)
+if %index% lss 2 goto :exit
 ffmpeg.exe %input% -filter_complex "amerge=inputs=%index%" %~dp1merged_audio_track%~x1
-:Exit
+:exit
 timeout /t 5
 exit
-:Merge
+:merge
 set /a index+=1
 set input=-i "%1" %input%
