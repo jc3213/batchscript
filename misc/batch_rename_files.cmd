6eb2f5e59a6d6ddfa271750894d1d8bf8790a078
jc3213 <jc3213@users.noreply.github.com>
2023-05-13 09:28:12 +0800

    Update batch_rename_files.cmd

--- misc/batch_rename_files.cmd
+++ misc/batch_rename_files.cmd
@@ -1,30 +1,28 @@
 @echo off
 if not exist %1 exit
 cd /d %1
-set /a seek=1
 for /f %%a in ('dir /b /a-d') do (call :main "%%a")
 :main
-echo ===========================================================
-:menst
-set filename=%~1
-if not defined filename goto :mened
-for /f "tokens=1* delims=-_.() " %%a in ("%filename%") do (call :menu "%%a" "%%b")
-:mened
-echo ===========================================================
+echo ============================================================
+:menu
+for /f "tokens=1* delims=-_.() " %%a in (%1) do (call :item "%%a" "%%b")
+:item
+set /a index+=1
+echo %~1| findstr /r "^[0-9][0-9]*$" >nul && set better=** || set better=
+echo %index%            ^>^>            %~1        %better%
+if %2 equ "" goto :mend
+call :menu %2
+:mend
+echo ============================================================
 :input
-set /p index=^> 
+set /p act=^> 
 echo.
-echo %index%| findstr /r "^[1-9][0-9]*$" >nul || goto :input
+if %act% gtr %index% goto :input
 for /f %%a in ('dir /b /a-d') do (call :rename "%%a")
 timeout /t 5
 exit
-:menu
-echo %~1| findstr /r "^[0-9][0-9]*$" >nul && set better=** || set better=
-echo %seek%            ^>^>            %~1        %better%
-set /a seek+=1
-call :menst %2
 :rename
-for /f "tokens=%index% delims=-_.() " %%a in (%1) do (call :exec %1 "%%a")
+for /f "tokens=%act% delims=-_.() " %%a in (%1) do (call :exec %1 "%%a")
 exit /b
 :exec
 set input=%~2
