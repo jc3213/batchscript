db1df0aa880bab981f1cab040d6502f8b585d038
jc3213 <jc3213@users.noreply.github.com>
2023-07-17 23:20:38 +0800

    Update move_qqtemp_to_ramdisk.cmd

--- QQ/move_qqtemp_to_ramdisk.cmd
+++ QQ/move_qqtemp_to_ramdisk.cmd
@@ -5,29 +5,34 @@ exit
 :main
 for /f %%a in ('wmic logicaldisk where "VolumeName='ramdisk'" get Caption ^| find ":"') do (set ramdisk=%%a\Temp)
 if not defined ramdisk goto :exit
+echo ======================================================
+echo Prevent installing QQ Guild? (Y/y)
+echo ======================================================
+set /p act=^> 
 for /f "tokens=2 delims==" %%a in ('wmic process where name^="qq.exe" get executablepath /value') do (set app=%%a)
 if defined app taskkill /f /im qq.exe
-set profile=%Public%\Documents\Tencent\QQ\UserDataInfo.ini
-if not exist "%profile%" goto :document
-for /f "tokens=1,2 delims==" %%a in ('type %profile%') do (
+set data=%public%\Documents\Tencent\QQ\UserDataInfo.ini
+set user=%userprofile%\Documents\Tencent Files
+set tenc=%appdata%\Tencent
+if not exist "%data%" goto :document
+for /f "tokens=1,2 delims==" %%a in ('type %data%') do (
     if %%a equ UserDataSavePathType set type=%%b
     if %%a equ UserDataSavePath set path=%%b
 )
-if %type% equ 1 goto :document
-if %type% equ 2 goto :defined
+if [%type%] equ [1] goto :document
+if [%type%] equ [2] goto :defined
 goto :exit
 :defined
+if not exist "%path%" md "%path%"
 cd /d %path%
-if %errorlevel% equ 0 goto :process
+goto :process
 :document
-cd /d %Userprofile%\Documents
-md "Tencent Files" 2>nul
-cd "Tencent Files"
+if not exist "%user%" md "%user%"
+cd /d %user%
 :process
 for /d %%a in (*) do (call :profile "%%a")
-cd /d %appdata%
-md Tencent 2>nul
-cd Tencent
+if not exist "%tenc%" md "%tenc%"
+cd /d %tenc%
 rd /s /q Logs 2>nul
 rd /s /q QQTempSys 2>nul
 rd /s /q QQ\Temp 2>nul
@@ -35,8 +40,12 @@ rd /s /q QQ\webkit_cache 2>nul
 mklink /d Logs %ramdisk%
 mklink /d QQTempSys %ramdisk%
 mklink /d QQ\webkit_cache %ramdisk%
-md QQ\Temp
-icacls QQ\Temp /deny Everyone:(F)
+if /i [%act%] equ [y] (
+    md QQ\Temp
+    icacls QQ\Temp /deny Everyone:(F)
+) else (
+    mklink /d QQ\Temp %ramdisk%
+)
 cd Users
 for /d %%a in (*) do (call :appdata "%%a")
 goto :exit
