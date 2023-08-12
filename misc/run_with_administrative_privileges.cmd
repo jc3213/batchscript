65396a39fe61d03aefe9fb365f663a2d0bdb997f
jc3213 <jc3213@users.noreply.github.com>
2023-08-12 21:39:24 +0800

    Update run_with_administrative_privileges.cmd

--- misc/run_with_administrative_privileges.cmd
+++ misc/run_with_administrative_privileges.cmd
@@ -1,5 +1,5 @@
 @echo off
-net session >nul 2>&1 && goto :main
+net session >nul 2>&1 && goto :admin
 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c pushd ""%~dp0"" && ""%~s0","","runas",1)(window.close)
 exit
 :admin
