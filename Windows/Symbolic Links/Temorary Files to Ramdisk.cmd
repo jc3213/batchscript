17e692677d3b2b69568debc1210048fedfefe4f0
jc3213 <jc3213@users.noreply.github.com>
2023-08-20 16:00:18 +0800

    Update Temorary Files to Ramdisk.cmd

--- Windows/Symbolic Links/Temorary Files to Ramdisk.cmd
+++ Windows/Symbolic Links/Temorary Files to Ramdisk.cmd
@@ -1,7 +1,9 @@
 @echo off
-for /f %%a in ('wmic logicaldisk where "VolumeName='RAMDISK'" get Caption ^| find ":"') do (set Ramdisk=%%a\Temp)
-rd /s /q "%LocalAppData%\Temp"
-rd /s /q "%SystemRoot%\Temp"
-mklink /d "%LocalAppData%\Temp" "%Ramdisk%"
-mklink /d "%SystemRoot%\Temp" "%Ramdisk%"
+for /f %%a in ('wmic logicaldisk where "VolumeName='RAMDISK'" get Caption ^| find ":"') do (set ramdisk=%%a\Temp)
+if not exist "%ramdisk%" goto :return
+rd /s /q "%localappdata%\Temp"
+rd /s /q "%systemroot%\Temp"
+mklink /d "%localappdata%\Temp" "%ramdisk%"
+mklink /d "%systemroot%\Temp" "%ramdisk%"
+:return
 timeout /t 5
