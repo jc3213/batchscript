4a9e3ad019c7dc98fd4b9811fef25ed23e199605
jc3213 <jc3213@users.noreply.github.com>
2023-12-11 00:20:56 +0800

    Update zip_for_manga.cmd

--- 7-zip/zip_for_manga.cmd
+++ 7-zip/zip_for_manga.cmd
@@ -8,17 +8,19 @@ set /p yes=^>
 for %%a in (%*) do (call :main %%a)
 goto :exit
 :main
+echo.
+echo 7-Zip is processing "%~nx1"
 cd /d %1 2>nul
 if %ErrorLevel% equ 0 goto :newzip
 :repack
-echo a|"%zip%" x %1 -o"%~dnp1"
-"%zip%" a "%~dnp1.zip" "%~dnp1\*"
+"%zip%" x %1 -o"%~dnp1" -aoa >nul
+"%zip%" a "%~dnp1.zip" "%~dnp1\*" >nul
 if /i [%yes%] neq [y] exit /b
 rd /s /q "%~dnp1"
 if /i %~X1 neq .zip del %1 /s /q
 exit /b
 :newzip
-"%zip%" a "%~dnpx1.zip" "*"
+"%zip%" a "%~dnpx1.zip" "*" >nul
 if /i [%yes%] neq [y] exit /b
 cd..
 rd /s /q "%~1"
