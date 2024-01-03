27298cc28eaa4024e05ffaf75588ec6bf0f707c6
jc3213 <jc3213@users.noreply.github.com>
2024-01-03 11:39:24 +0800

    Update Manage Microsoft Windows.ps1

--- Microsoft/Manage Microsoft Windows.ps1
+++ Microsoft/Manage Microsoft Windows.ps1
@@ -9,23 +9,23 @@ function Manage-WindowsPowerPlan {
     Write-Host "5. Manage Heterogeneous Thread Policy"
     Write-Host "+. Return to main menu"
     Write-Host "==============================================================="
-    $msedge = Read-Host ">"
+    $winpower = Read-Host ">"
 
-    switch ($msedge) {
+    switch ($winpower) {
         "1" {
-            Windows-PowerHibernation
+            Power-ManageHibernation
         }
         "2" {
-            Windows-DiskIdleTimeout
+            Power-DiskIdleTimeout
         }
         "3" {
-            Windows-MaximumPState
+            Power-MaximumPState
         }
         "4" {
-            Windows-MinimumPState
+            Power-MinimumPState
         }
         "5" {
-            Windows-HeterogeneousThread
+            Power-HeterogeneousThread
         }
         "+" {
         }
@@ -35,7 +35,7 @@ function Manage-WindowsPowerPlan {
     }
 }
 
-function Windows-PowerHibernation {
+function Power-ManageHibernation {
     Clear-Host
     Write-Host "System Hibernation - Power Plan"
     Write-Host "=================================================================="
@@ -56,12 +56,12 @@ function Windows-PowerHibernation {
             Manage-WindowsPowerPlan
         }
         default {
-            Windows-PowerHibernation
+            Power-ManageHibernation
         }
     }
 }
 
-function Windows-DiskIdleTimeout {
+function Power-DiskIdleTimeout {
     Clear-Host
     Write-Host "Disk Idle Timeout - Power Plan"
     Write-Host "=================================================================="
@@ -84,12 +84,12 @@ function Windows-DiskIdleTimeout {
             Manage-WindowsPowerPlan
         }
         default {
-            Windows-DiskIdleTimeout
+            Power-DiskIdleTimeout
         }
     }
 }
 
-function Windows-MaximumPState {
+function Power-MaximumPState {
     Clear-Host
     Write-Host "Processor Maximum P-state - Power Plan"
     Write-Host "=================================================================="
@@ -108,7 +108,7 @@ function Windows-MaximumPState {
     powercfg /setactive scheme_current
 }
 
-function Windows-MinimumPState {
+function Power-MinimumPState {
     Clear-Host
     Write-Host "Processor Minimum P-state - Power Plan"
     Write-Host "=================================================================="
@@ -127,7 +127,7 @@ function Windows-MinimumPState {
     powercfg /setactive scheme_current
 }
 
-function Windows-HeterogeneousThread {
+function Power-HeterogeneousThread {
     Clear-Host
     Write-Host "Heterogeneous Thread Policy - Power Plan"
     Write-Host "=================================================================="
@@ -152,7 +152,400 @@ function Windows-HeterogeneousThread {
             Manage-WindowsPowerPlan
         }
         default {
-            Windows-HeterogeneousThread
+            Power-HeterogeneousThread
+        }
+    }
+}
+
+function Manage-WindowsUpdate {
+    Clear-Host
+    Write-Host "Manage Windows Update"
+    Write-Host "==============================================================="
+    Write-Host "1. Manage Auto Update"
+    Write-Host "2. Manage Driver Auto Update"
+    Write-Host "3. Manage Windows Update Service (wuauserv)"
+    Write-Host "+. Return to Main Menu"
+    Write-Host "==============================================================="
+    $winupdate = Read-Host ">"
+
+    switch ($winupdate) {
+        "1" {
+            WinUpdate-AutoUpdate
+        }
+        "2" {
+            WinUpdate-DriverAutoUpdate
+        }
+        "3" {
+            WinUpdate-UpdateService
+        }
+        "+" {
+        }
+        default {
+            Manage-WindowsUpdate
+        }
+    }
+}
+
+function WinUpdate-AutoUpdate {
+    Clear-Host
+    Write-Host "Auto Update - Windows Update"
+    Write-Host "==============================================================="
+    Write-Host "0. Disable"
+    Write-Host "1. Enable (Default)"
+    Write-Host "+. Return to Upper Menu"
+    Write-Host "==============================================================="
+    $update_autoup = Read-Host ">"
+
+    switch ($update_autoup) {
+        "0" {
+            Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "NoAutoUpdate" -Value 1
+        }
+        "1" {
+            Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "NoAutoUpdate"
+        }
+        "+" {
+            Manage-WindowsUpdate
+        }
+        default {
+            WinUpdate-AutoUpdate
+        }
+    }
+}
+
+function WinUpdate-DriverAutoUpdate {
+    Clear-Host
+    Write-Host "Driver Auto Update - Windows Update"
+    Write-Host "==============================================================="
+    Write-Host "0. Disable"
+    Write-Host "1. Enable (Default)"
+    Write-Host "+. Return to Upper Menu"
+    Write-Host "==============================================================="
+    $update_driver = Read-Host ">"
+
+    switch ($update_driver) {
+        "0" {
+            Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "ExcludeWUDriversInQualityUpdate" -Value 1
+        }
+        "1" {
+            Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate" -Name "ExcludeWUDriversInQualityUpdate"
+        }
+        "+" {
+            Manage-WindowsUpdate
+        }
+        default {
+            WinUpdate-DriverAutoUpdate
+        }
+    }
+}
+
+function WinUpdate-UpdateService {
+    Clear-Host
+    Write-Host "Windows Update Service - Windows Update"
+    Write-Host "==============================================================="
+    Write-Host "0. Disable"
+    Write-Host "1. Enable (Default)"
+    Write-Host "+. Return to Upper Menu"
+    Write-Host "==============================================================="
+    $update_service = Read-Host ">"
+
+    switch ($update_service) {
+        "0" {
+            Stop-Service -Name "wuauserv" -Force
+            Set-Service -Name "wuauserv" -StartupType Disabled
+        }
+        "1" {
+            Set-Service -Name "wuauserv" -StartupType Automatic
+            Start-Service -Name "wuauserv"
+        }
+        "+" {
+            Manage-WindowsUpdate
+        }
+        default {
+            WinUpdate-UpdateService
+        }
+    }
+}
+
+function Manage-WindowsMaintenance {
+    Clear-Host
+    Write-Host "Manage Windows Maintenance"
+    Write-Host "==============================================================="
+    Write-Host "1. Manage Super Prefetch"
+    Write-Host "2. Manage Disk Defragment"
+    Write-Host "3. Manage Diagnostic"
+    Write-Host "4. Manage Auto Maintenance"
+    Write-Host "+. Return to Main Menu"
+    Write-Host "==============================================================="
+    $winmain = Read-Host ">"
+
+    switch ($winmain) {
+        "1" {
+            WinMain-SuperPrefetch
+        }
+        "2" {
+            WinMain-DiskDefragment
+        }
+        "3" {
+            WinMain-Diagnostic
+        }
+        "4" {
+            WinMain-AutoMaintenance
+        }
+        "+" {
+        }
+        default {
+            Manage-WindowsMaintenance
+        }
+    }
+}
+
+function WinMain-SuperPrefetch {
+    Clear-Host
+    Write-Host "Super Prefetch - Windows Maintenance"
+    Write-Host "==============================================================="
+    Write-Host "0. Disable"
+    Write-Host "1. Enable (Default)"
+    Write-Host "+. Return to Upper Menu"
+    Write-Host "==============================================================="
+    $main_prefetch = Read-Host ">"
+
+    switch ($main_prefetch) {
+        "0" {
+            Stop-Service -Name "SysMain" -Force
+            Set-Service -Name "SysMain" -StartupType Disabled
+        }
+        "1" {
+            Set-Service -Name "SysMain" -StartupType Automatic
+            Start-Service -Name "SysMain"
+        }
+        "+" {
+            Manage-WindowsMaintenance
+        }
+        default {
+            WinMain-SuperPrefetch
+        }
+    }
+}
+
+function WinMain-DiskDefragment {
+    Clear-Host
+    Write-Host "Disk Defragment - Windows Maintenance"
+    Write-Host "==============================================================="
+    Write-Host "0. Disable"
+    Write-Host "1. Enable (Default)"
+    Write-Host "+. Return to Upper Menu"
+    Write-Host "==============================================================="
+    $main_defrag = Read-Host ">"
+
+    switch ($main_defrag) {
+        "0" {
+            schtasks /change /disable /tn "\Microsoft\Windows\Defrag\ScheduledDefrag"
+        }
+        "1" {
+            schtasks /change /enable /tn "\Microsoft\Windows\Defrag\ScheduledDefrag"
+        }
+        "+" {
+            Manage-WindowsMaintenance
+        }
+        default {
+            WinMain-DiskDefragment
+        }
+    }
+}
+
+function WinMain-Diagnostic {
+    Clear-Host
+    Write-Host "Diagnostic - Windows Maintenance"
+    Write-Host "==============================================================="
+    Write-Host "0. Disable"
+    Write-Host "1. Enable (Default)"
+    Write-Host "+. Return to Upper Menu"
+    Write-Host "==============================================================="
+    $main_diag = Read-Host ">"
+
+    switch ($main_diag) {
+        "0" {
+            schtasks /change /disable /tn "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
+            schtasks /change /disable /tn "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator"
+            Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry" -Value 0
+            Stop-Service -Name "DiagTrack" -Force
+            Set-Service -Name "DiagTrack" -StartupType Disabled
+            Stop-Service -Name "DPS" -Force
+            Set-Service -Name "DPS" -StartupType Disabled
+        }
+        "1" {
+            schtasks /change /enable /tn "\Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser"
+            schtasks /change /enable /tn "\Microsoft\Windows\Customer Experience Improvement Program\Consolidator"
+            Remove-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection" -Name "AllowTelemetry"
+            Set-Service -Name "DiagTrack" -StartupType Automatic
+            Start-Service -Name "DiagTrack"
+            Set-Service -Name "DPS" -StartupType Automatic
+            Start-Service -Name "DPS"
+        }
+        "+" {
+            Manage-WindowsMaintenance
+        }
+        default {
+            WinMain-Diagnostic
+        }
+    }
+}
+
+function WinMain-AutoMaintenance {
+    Clear-Host
+    Write-Host "Auto Maintenance - Windows Maintenance"
+    Write-Host "==============================================================="
+    Write-Host "0. Disable"
+    Write-Host "1. Enable (Default)"
+    Write-Host "+. Return to Upper Menu"
+    Write-Host "==============================================================="
+    $main_automain = Read-Host ">"
+
+    switch ($main_automain) {
+        "0" {
+            Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Maintenance" -Name "MaintenanceDisabled" -Value 1
+        }
+        "1" {
+            Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Schedule\Maintenance" -Name "MaintenanceDisabled"
+        }
+        "+" {
+            Manage-WindowsMaintenance
+        }
+        default {
+            WinMain-AutoMaintenance
+        }
+    }
+}
+
+function Manage-WindowsAccessibility {
+    Clear-Host
+    Write-Host "Windows Accessibility"
+    Write-Host "==============================================================="
+    Write-Host "1. Move Temporary Files to Ramdisk"
+    Write-Host "2. Context Menu (Windows 11)"
+    Write-Host "3. Windows Picture Viewer"
+    Write-Host "4. CPU Microcode Update"
+    Write-Host "+. Return to Main Menu"
+    Write-Host "==============================================================="
+    $winaccess = Read-Host ">"
+
+    switch ($winaccess) {
+        "1" {
+            WinAccess-TemporayToRamdisk
+        }
+        "2" {
+            WinAccess-ContextMenu
+        }
+        "3" {
+            WinAccess-PictureViewer
+        }
+        "4" {
+            WinAccess-CpuMicrocodeUpdate
+        }
+        "+" {
+        }
+        default {
+            Manage-WindowsAccessibility
+        }
+    }
+}
+
+function WinAccess-TemporayToRamdisk {
+    Clear-Host
+    Write-Host "Move Temporary Files to Ramdisk - Windows Accessibility"
+    Write-Host "==============================================================="
+    Write-Host "Please wait..."
+    Write-Host "==============================================================="
+
+    $ramdisk = Get-WmiObject -Query "SELECT Caption FROM Win32_LogicalDisk WHERE VolumeName='RAMDISK'" | Select-Object -ExpandProperty Caption
+    $ramdisk = Join-Path -Path $ramdisk -ChildPath "Temp"
+
+    if (Test-Path -Path $ramdisk) {
+        Remove-Item -Path "$env:LOCALAPPDATA\Temp" -Recurse -Force
+        Remove-Item -Path "$env:WINDIR\Temp" -Recurse -Force
+
+        New-Item -ItemType Junction -Path "$env:LOCALAPPDATA\Temp" -Value $ramdisk
+        New-Item -ItemType Junction -Path "$env:WINDIR\Temp" -Value $ramdisk
+    }
+}
+
+function WinAccess-ContextMenu {
+    Clear-Host
+    Write-Host "Context Menu (Windows 11) - Windows Accessibility"
+    Write-Host "==============================================================="
+    Write-Host "0. Modern Mode (Default)"
+    Write-Host "1. Legacy Mode"
+    Write-Host "+. Return to Upper Menu"
+    Write-Host "==============================================================="
+    $access_ctxmenu = Read-Host ">"
+
+    switch ($access_ctxmenu) {
+        "0" {
+            Remove-ItemProperty -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}" -ErrorAction SilentlyContinue
+        }
+        "1" {
+            New-Item -Path "HKCU:\Software\Classes\CLSID\{86ca1aa0-34aa-4e8b-a509-50c905bae2a2}\InprocServer32" -Force | Out-Null
+        }
+        "+" {
+            Manage-WindowsAccessibility
+        }
+        default {
+            WinAccess-ContextMenu
+        }
+    }
+}
+
+function WinAccess-PictureViewer {
+    Clear-Host
+    Write-Host "Windows Picture Viewer - Windows Accessibility"
+    Write-Host "==============================================================="
+    Write-Host "Processing..."
+    Write-Host "==============================================================="
+
+    $viewer = Join-Path -Path $PSScriptRoot -ChildPath "Picture Viewer.reg"
+
+    Invoke-WebRequest -Uri "https://raw.githubusercontent.com/jc3213/batchscript/main/Windows/Picture%20Viewer/Picture%20Viewer.reg" -OutFile $viewer
+    Start-Process -FilePath "regedit.exe" -ArgumentList "/s `"$viewer`"" -Verb RunAs
+
+    Write-Host "Do you want to delete the downloaded `"$viewer`" file? (y/n)"
+    $confirm = Read-Host ">"
+
+    if ($confirm -eq "y") {
+        Remove-Item -Path $viewer -Force
+    }
+}
+
+function WinAccess-CpuMicrocodeUpdate {
+    Clear-Host
+    Write-Host "CPU Microcode Update - Windows Accessibility"
+    Write-Host "==============================================================="
+    Write-Host "0. Restore from Backup"
+    Write-Host "1. Remove and Backup"
+    Write-Host "+. Return to Upper Menu"
+    Write-Host "==============================================================="
+    $avsub = Read-Host ">"
+
+    $amd = Join-Path -Path $env:WINDIR -ChildPath "System32\mcupdate_GenuineIntel.dll"
+    $intel = Join-Path -Path $env:WINDIR -ChildPath "System32\mcupdate_AuthenticAMD.dll"
+    $mcbk = Join-Path -Path $PSScriptRoot -ChildPath "microcode_backup.zip"
+
+    switch ($avsub) {
+        "0" {
+            Expand-Archive -Force -Path $mcbk -DestinationPath "$env:WINDIR\System32"
+        }
+        "1" {
+            takeown /f $amd
+            icacls $amd /grant "Administrators:F"
+            takeown /f $intel
+            icacls $intel /grant "Administrators:F"
+            Compress-Archive -Force -Path $amd, $intel -DestinationPath $mcbk
+            Remove-Item -Path $amd, $intel -Force
+        }
+        "+" {
+            Manage-WindowsAccessibility
+        }
+        default {
+            WinAccess-CpuMicrocodeUpdate
         }
     }
 }
@@ -514,13 +907,13 @@ while ($true) {
             Manage-WindowsPowerPlan
         }
         "2" {
-            
+            Manage-WindowsUpdate
         }
         "3" {
-            
+            Manage-WindowsMaintenance
         }
         "4" {
-            
+            Manage-WindowsAccessibility
         }
         "5" {
             Manage-MicrosoftEdge
