9a79c1a432212402752a1c8a1407fb466956025c
jc3213 <jc3213@users.noreply.github.com>
2024-01-06 22:26:37 +0800

    Update upscale_ncnn_utilities.ps1

--- upscale/upscale_ncnn_utilities.ps1
+++ upscale/upscale_ncnn_utilities.ps1
@@ -68,36 +68,6 @@ function Set-TTA {
     }
 }
 
-function Real-ESRGAN {
-    Set-Format
-    Get-Files
-    foreach ($file in $script:files) {
-        Write-Host "`n`nReal-ESRGAN is processing: `"$file`""
-        $folder = Split-Path -Path $file -Parent
-        $name = [System.IO.Path]::GetFileNameWithoutExtension($file) + "$script:name.$script:format"
-        $output = "$folder\$name"
-        Start-Process -FilePath $realesrgan -ArgumentList "-i `"$file`" -o `"$output`" $script:params" -Wait -WindowStyle Hidden
-        Write-Host "Output file: `"$output`""
-    }
-    Pause
-}
-
-function Waifu-2x {
-    Set-Scale
-    Set-Denoise
-    Set-Format
-    Get-Files
-    foreach ($file in $script:files) {
-        Write-Host "`n`nWaifu2x is processing: `"$file`""
-        $folder = Split-Path -Path $file -Parent
-        $name = [System.IO.Path]::GetFileNameWithoutExtension($file) + "$script:name.$script:format"
-        $output = "$folder\$name"
-        Start-Process -FilePath $waifu2x -ArgumentList "-i `"$file`" -o `"$output`" $script:params" -Wait -WindowStyle Hidden
-        Write-Host "Output file: `"$output`""
-    }
-    Pause
-}
-
 function Get-Files {
     Add-Type -AssemblyName System.Windows.Forms
     $dialog = New-Object System.Windows.Forms.OpenFileDialog
@@ -110,6 +80,35 @@ function Get-Files {
     }
 }
 
+function Real-ESRGAN {
+    Set-Format
+    Get-Files
+    Upscale-Output
+}
+
+function Waifu-2x {
+    Set-Scale
+    Set-Denoise
+    Set-Format
+    Get-Files
+    Upscale-Output
+}
+
+function Upscale-Output {
+    Write-Host "`n`nUpscaler     :   $script:worker"
+    Write-Host "Model        :   $script:model"
+    Write-Host "Scale Ratio  :   $script:scale`x"
+    foreach ($file in $script:files) {
+        Write-Host "`n`nProcessing: `"$file`""
+        $folder = Split-Path -Path $file -Parent
+        $name = [System.IO.Path]::GetFileNameWithoutExtension($file) + " $script:name.$script:format"
+        $output = "$folder$name"
+        Start-Process -FilePath $script:app -ArgumentList "-i `"$file`" -o `"$output`" $script:params" -Wait -WindowStyle Hidden
+        Write-Host "Output file: `"$output`""
+    }
+    Pause
+}
+
 while ($true) {
     Clear-Host
     Write-Host "============================================================"
@@ -120,35 +119,55 @@ while ($true) {
     Write-Host "5. Waifu2x Up-convert RGB"
     Write-Host "6. Waifu2x Up-convert Photo"
     Write-Host "============================================================"
-    $model = Read-Host "> "
+    $model = Read-Host ">"
     switch ($model) {
         "1" {
+            $script:app = $realesrgan
+            $script:model = "x4plus"
+            $script:scale = "4"
+            $script:worker = "Real-ESRGAN"
             $script:name = "(real-esrgan)(x4plus)(4x)"
             $script:params = "-n realesrgan-x4plus"
             Real-ESRGAN
         }
         "2" {
+            $script:app = $realesrgan
+            $script:model = "x4plus-anime"
+            $script:scale = "4"
+            $script:worker = "Real-ESRGAN"
             $script:name = "(real-esrgan))(x4plus-anime)(4x)"
             $script:params = "-n realesrgan-x4plus-anime"
             Real-ESRGAN
         }
         "3" {
+            $script:app = $realesrgan
+            $script:model = "animevideov3"
+            $script:worker = "Real-ESRGAN"
             $script:name = "(real-esrgan)(animevideov3)"
             $script:params = "-n realesr-animevideov3"
             Set-Scale
             Real-ESRGAN
         }
         "4" {
-            $script:name = "(waifu2x)(cu-net)"
+            $script:app = $waifu2x
+            $script:model = "cunet"
+            $script:worker = "Waifu2x"
+            $script:name = "(waifu2x)(cunet)"
             $script:params = "-m models-cunet"
             Waifu-2x
         }
         "5" {
+            $script:app = $waifu2x
+            $script:model = "upconv_7_anime_style_art_rgb"
+            $script:worker = "Waifu2x"
             $script:name = "(waifu2x)(upconv_7_anime_style_art_rgb)"
             $script:params = "-m models-upconv_7_anime_style_art_rgb"
             Waifu-2x
         }
         "6" {
+            $script:app = $waifu2x
+            $script:model = "upconv_7_photo"
+            $script:worker = "Waifu2x"
             $script:name = "(waifu2x)(upconv_7_photo)"
             $script:params = "-m models-upconv_7_photo"
             Waifu-2x
