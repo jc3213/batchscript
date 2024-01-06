62b01a5f5f768e479c47bf4231ff1438cf8cc917
jc3213 <jc3213@users.noreply.github.com>
2024-01-06 13:24:38 +0800

    Update magick_utilities.cmd

--- magick/magick_utilities.cmd
+++ magick/magick_utilities.cmd
@@ -1,7 +1,7 @@
 @echo off
 title ImageMagick Utilities
 set imagick=%~dp0bin\magick.exe
-:Main
+:menu
 cls
 echo ============================================================
 echo 1. Crop with area
@@ -10,28 +10,36 @@ echo 3. Convert images
 echo 4. Darken images
 echo ============================================================
 set /p op=^> 
-if [%op%] equ [1] goto :Crop
-if [%op%] equ [2] goto :Shave
-if [%op%] equ [3] goto :Conv
-if [%op%] equ [4] goto :Dark
-goto :Main
-:Crop
-call :Area
-for %%a in (%*) do (call :Process "%%~a" crop)
-goto :Exit
-:Shave
-call :Area
-for %%a in (%*) do (call :Process "%%~a" shave)
-goto :Exit
-:Conv
-call :Format
-for %%a in (%*) do (call :Convert "%%~a")
-goto :Exit
-:Dark
-call :Level
-for %%a in (%*) do (call :Darken "%%~a")
-goto :Exit
-:Area
+if [%op%] equ [1] goto :crop
+if [%op%] equ [2] goto :shave
+if [%op%] equ [3] goto :format
+if [%op%] equ [4] goto :darken
+goto :menu
+:crop
+call :area
+set name=cropped_
+set params=-crop %area%
+for %%a in (%*) do (call :process "%%~a" convert)
+goto :exit
+:shave
+call :area
+set name=cutted_
+set params=-shave %area%
+for %%a in (%*) do (call :process "%%~a" convert)
+goto :exit
+:format
+call :output
+set name=output%qu%_
+set params=-quality %qu%
+for %%a in (%*) do (call :process "%%~a")
+goto :exit
+:darken
+call :level
+set name=darken%lv%_
+set params=-level %lv%%%,100%%
+for %%a in (%*) do (call :process "%%~a")
+goto :exit
+:area
 echo.
 echo.
 echo ============================================================
@@ -39,24 +47,12 @@ echo https://imagemagick.org/script/command-line-processing.php#geometry
 echo Sample: 300x100 (width x height)
 echo Cut left and right: 300px(width), cut top and bottom: 100px(height)
 echo Sample: 300x100+20+30 (width x height + left + top)
-echo Crop image area start from: left 20px to 320px, top: 30px to 130px
+echo Crop image area start from: left 20px to 320px, top 30px to 130px
 echo ============================================================
 set /p area=^> 
-if not defined area goto :Area
-echo.
-echo.
-echo ImageMagick is processing images...
+if not defined area goto :area
 exit /b
-:Process
-cd /d %1 2>nul
-if %errorlevel% equ 0 goto :ProcessFolder
-"%imagick%" convert %1 -%2 %area% "%~dp1cutted_%~nx1"
-exit /b
-:ProcessFolder
-md "%~dp1cutted_%~nx1" 2>nul
-for %%a in (*) do ("%imagick%" convert "%%~a" -%2 %area% "%~dp1cutted_%~nx1\%%~nxa")
-exit /b
-:Format
+:output
 echo.
 echo.
 echo ============================================================
@@ -69,7 +65,7 @@ if [%fm%] equ [1] set format=jpg
 if [%fm%] equ [2] set format=png
 if [%fm%] equ [3] set format=avif
 if not defined format goto :Format
-:Quality
+:quality
 echo.
 echo.
 echo ============================================================
@@ -78,20 +74,8 @@ echo Default: 90
 echo ============================================================
 set /p qu=^> 
 echo %qu%| findstr /r "^[1-9]$ ^[1-9][0-9]$ ^100$" >nul || set qu=90
-echo.
-echo.
-echo ImageMagick is converting images...
 exit /b
-:Convert
-cd /d %1 2>nul
-if %errorlevel% equ 0 goto :ConvertFolder
-"%imagick%" %1 -quality %qu% "%~dp1conv_%~n1.%format%"
-exit /b
-:ConvertFolder
-md "%~dp1conv_%~nx1" 2>nul
-for %%a in (*) do ("%imagick%" "%%~a" -quality %qu% "%~dp1conv_%~nx1\%%~na.%format%")
-exit /b
-:Level
+:level
 echo.
 echo.
 echo ============================================================
@@ -100,19 +84,26 @@ echo Default: 30
 echo ============================================================
 set /p lv=^> 
 echo %lv%| findstr /r "^[1-9]$ ^[1-9][0-9]$ ^100$" >nul || set lv=30
-echo.
-echo.
-echo ImageMagick is darkening images...
 exit /b
-:Darken
+:process
+echo.
+echo.
 cd /d %1 2>nul
-if %errorlevel% equ 0 goto :DarkenFolder
-"%imagick%" %1 -level %lv%%%,100%% "%~dp1dark_%~nx1"
+if %errorlevel% equ 0 goto :folder
+call :imagick %1 %2
 exit /b
-:DarkenFolder
-md "%~dp1dark_%~nx1" 2>nul
-for %%a in (*) do ("%imagick%" "%%~a" -level %lv%%%,100%% "%~dp1dark_%~nx1\%%~nxa")
+:folder
+set folder=%~dp1%name%%~nx1
+md "%folder%" 2>nul
+for %%a in (*) do (call :imagick "%%~a" %2)
+set folder=
 exit /b
-:Exit
+:imagick
 echo.
+echo ImageMagick is processing: %1
+if defined qu (set output=%~n1.%format%) else (set output=%~nx1)
+if defined folder (set output=%folder%\%output%) else (set output=%~dp1%name%%output%)
+if defined qu ("%imagick%" %2 %1 %params% "%~dp1%name%%~n1.%format%") else (%imagick%" %2 %1 %params% "%~dp1%name%%~nx1")
+exit /b
+:exit
 timeout /t 5
