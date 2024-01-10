070fcdda94b90dcdc181d3197e2b63eceb5d2b23
jc3213 <jc3213@users.noreply.github.com>
2024-01-10 22:13:25 +0800

    Create refresh_icon_caches.cmd

---
+++ misc/refresh_icon_caches.cmd
@@ -0,0 +1,16 @@
+@echo off
+net session >nul 2>&1 && goto :admin
+mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" %*","%~dp0","runas",1)(window.close)
+exit
+:admin
+taskkill /f /im "explorer.exe"
+cd /d %localappdata%\Microsoft\Windows\Explorer
+for %%a in (*.*) do (call :refresh "%%~a")
+start "" "explorer.exe"
+timeout /t 5
+exit
+:refresh
+takeown /f %1
+icacls %1 /grant Administrators:F
+attrib -h -s -r %1
+del /f /q /a %1
