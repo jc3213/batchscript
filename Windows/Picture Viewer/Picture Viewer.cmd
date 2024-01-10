a1bd74c19fd27a07da45ecb7dcb87f64f9774349
jc3213 <jc3213@users.noreply.github.com>
2024-01-10 19:55:19 +0800

    Update Picture Viewer.cmd

--- Windows/Picture Viewer/Picture Viewer.cmd
+++ Windows/Picture Viewer/Picture Viewer.cmd
@@ -1,54 +1,29 @@
 @echo off
-call :avm1app1 bmp Bitmap
-call :avm1app1 dib Bitmap
-call :avm1app1 gif Gif
-call :avm1app1 jfif JFIF
-call :avm1app1 jpe Jpeg
-call :avm1app1 jpeg Jpeg
-call :avm1app1 jpg Jpeg
-call :avm1app1 png Png
-call :avm1app1 tiff Tiff
-call :avm1app1 tif Tiff
-call :avm1app1 wdp Wdp
-call :avm1app2 Bitmap 70
-call :avm1app2 JFIF 72
-call :avm1app2 Jpeg 72
-call :avm1app2 Png 71
-call :avm1app2 Tiff 122
-call :avm1app2 Wdp
-call :avm1app4 Bitmap 6
-call :avm1app4 JFIF 5
-call :avm1app4 Jpeg 5
-call :avm1app4 Png 7
-call :avm1app4 Tiff 8
-call :avm1app5 JFIF
-call :avm1app5 Jpeg
-call :avm1app5 Wdp
-call :avm1app6 JFIF
-call :avm1app6 Jpeg
-call :avm1app6 Tiff
-call :avm1app6 Wdp
-timeout /t 5
+set viewasso="bmp Bitmap" "dib Bitmap" "gif Gif" "jfif JFIF" "jpe Jpeg" "jpeg Jpeg" "jpg Jpeg" "png Png" "tiff Tiff" "tif Tiff" "wdp Wdp"
+for %%a in (%viewasso%) do (for /f "tokens=1-2 delims= " %%i in (%%a) do (call :miscm1asso %%i %%j))
+set viewcomm="Bitmap imageres.dll,70" "JFIF imageres.dll,72" "Jpeg imageres.dll,72" "Png imageres.dll,71" "Tiff imageres.dll,122" "Wdp wmphoto.dll,-400"
+for %%a in (%viewcomm%) do (for /f "tokens=1-2 delims= " %%i in (%%a) do (call :miscm1comm %%i %%j))
+set viewicon="Bitmap 6" "JFIF 5" "Jpeg 5" "Png 7" "Tiff 8"
+for %%a in (%viewicon%) do (for /f "tokens=1-2 delims= " %%i in (%%a) do (call :miscm1icon %%i %%j))
+for %%a in (JFIF Jpeg Wdp) do (call :miscm1edit %%a)
+for %%a in (JFIF Jpeg Tiff Wdp) do (call :miscm1open %%a)
+timeout /5
 exit
-:avm1app1
+:miscm1asso
 reg add "HKLM\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /v ".%1" /t "REG_SZ" /d "PhotoViewer.FileAssoc.%2" /f
 exit /b
-:avm1app2
+:miscm1comm
 reg add "HKCR\PhotoViewer.FileAssoc.%1" /v "ImageOptionFlags" /t "REG_DWORD" /d "0x00000001" /f
 reg add "HKCR\PhotoViewer.FileAssoc.%1\shell\open\command" /ve /t "REG_EXPAND_SZ" /d "%%SystemRoot%%\System32\rundll32.exe \"%%ProgramFiles%%\Windows Photo Viewer\PhotoViewer.dll\", ImageView_Fullscreen %%1" /f
 reg add "HKCR\PhotoViewer.FileAssoc.%1\shell\open\DropTarget" /v "Clsid" /t "REG_SZ" /d "{FFE2A43C-56B9-4bf5-9A79-CC6D4285608A}" /f
-if %1 equ Wdp goto :avm1app3
-reg add "HKCR\PhotoViewer.FileAssoc.%1\DefaultIcon" /ve /t "REG_SZ" /d "%%SystemRoot%%\System32\imageres.dll,-%2" /f
+reg add "HKCR\PhotoViewer.FileAssoc.%1\DefaultIcon" /ve /t "REG_SZ" /d "%%SystemRoot%%\System32\%2" /f
 exit /b
-:avm1app3
-reg add "HKCR\PhotoViewer.FileAssoc.Wdp\DefaultIcon" /ve /t "REG_SZ" /d "%%SystemRoot%%\System32\wmphoto.dll,-400" /f
-exit /b
-:avm1app4
+:miscm1icon
 reg add "HKCR\PhotoViewer.FileAssoc.%1" /v "FriendlyTypeName" /t "REG_EXPAND_SZ" /d "@%%ProgramFiles%%\Windows Photo Viewer\PhotoViewer.dll,-305%2" /f
 exit /b
-:avm1app5
+:miscm1edit
 reg add "HKCR\PhotoViewer.FileAssoc.%1" /v "EditFlags" /t "REG_DWORD" /d "0x00010000" /f
 exit /b
-:avm1app6
+:miscm1open
 reg add "HKCR\PhotoViewer.FileAssoc.%1\shell\open" /v "MuiVerb" /t "REG_EXPAND_SZ" /d "@%%ProgramFiles%%\Windows Photo Viewer\photoviewer.dll,-3043" /f
 exit /b
