4a27f10d53285aa70c3be3df8f3fb319c0b6dec7
jc3213 <jc3213@users.noreply.github.com>
2024-01-13 22:47:40 +0800

    Update install.ps1

--- Windows/LTSC 2021 AV1 Video Extension/install.ps1
+++ Windows/LTSC 2021 AV1 Video Extension/install.ps1
@@ -1,8 +1,7 @@
-$CurrentDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 if ([Environment]::Is64BitOperatingSystem) {
     $SystemArch = "x64"
 } else {
     $SystemArch = "x86"
 }
-Add-AppxPackage -Path "$CurrentDir\Microsoft.AV1VideoExtension_1.1.52851.0_$SystemArch`__8wekyb3d8bbwe.Appx"
+Add-AppxPackage -Path "$PSScriptRoot\Microsoft.AV1VideoExtension_1.1.52851.0_$SystemArch`__8wekyb3d8bbwe.Appx"
 Pause
