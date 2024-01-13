6513e0fd150e79137f7ad20ef32e9b5fa5f4efd1
jc3213 <jc3213@users.noreply.github.com>
2024-01-13 22:07:16 +0800

    Update Microsoft Windows Tweaks.cmd

--- Microsoft/Microsoft Windows Tweaks.cmd
+++ Microsoft/Microsoft Windows Tweaks.cmd
@@ -1,12 +1,12 @@
 @echo off
-net session >nul 2>&1 && goto :admin
+net session >nul 2>&1 && goto :runasadmin
 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" %*","","runas",1)(window.close)
 exit
-:admin
+:runasadmin
 set ppmenu="%~dp0Windows Power Plan.cmd"
 set wumenu="%~dp0Windows Update.cmd"
 set egmenu="%~dp0Microsoft Edge.cmd"
-set demenu="%~dp0Windows Defender.cmd"
+set demenu="%~dp0Microsoft Defender.cmd"
 set mnmenu="%~dp0Windows Maintenance.cmd"
 set avmenu="%~dp0Windows Accessibility.cmd"
 :mainmenu
@@ -16,7 +16,7 @@ echo ==================================================================
 if exist %ppmenu% echo 1. Windows Power Plan
 if exist %wumenu% echo 2. Windows Update
 if exist %egmenu% echo 3. Microsoft Edge
-if exist %demenu% echo 4. Windows Defender
+if exist %demenu% echo 4. Microsoft Defender
 if exist %mnmenu% echo 5. Windows Maintenance
 if exist %avmenu% echo 6. Windows Accessibility
 echo ==================================================================
