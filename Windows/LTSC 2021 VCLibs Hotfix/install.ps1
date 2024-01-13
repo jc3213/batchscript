2610f385d5a3e4f5de748d9ce2a0f735e53a17e4
jc3213 <jc3213@users.noreply.github.com>
2024-01-13 22:46:55 +0800

    Update install.ps1

--- Windows/LTSC 2021 VCLibs Hotfix/install.ps1
+++ Windows/LTSC 2021 VCLibs Hotfix/install.ps1
@@ -1,8 +1,7 @@
-$CurrentDir = Split-Path -Parent $MyInvocation.MyCommand.Path
 if ([Environment]::Is64BitOperatingSystem) {
     $SystemArch = "x64"
 } else {
     $SystemArch = "x86"
 }
-Add-AppxPackage -Path "$CurrentDir\Microsoft.VCLibs.140.00_14.0.30704.0_$SystemArch`__8wekyb3d8bbwe.Appx"
+Add-AppxPackage -Path "$PSScriptRoot\Microsoft.VCLibs.140.00_14.0.30704.0_$SystemArch`__8wekyb3d8bbwe.Appx"
 Pause
