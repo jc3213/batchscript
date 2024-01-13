fe243459f610d7e86628a4017e61467e3a3b9d4c
jc3213 <jc3213@users.noreply.github.com>
2024-01-13 22:42:36 +0800

    Update install.ps1

--- Windows/LTSC 2021 VCLibs Hotfix/install.ps1
+++ Windows/LTSC 2021 VCLibs Hotfix/install.ps1
@@ -4,5 +4,5 @@ if ([Environment]::Is64BitOperatingSystem) {
 } else {
     $SystemArch = "x86"
 }
-Add-AppxPackage -Path "$CurrentDir\Microsoft.VCLibs.140.00_14.0.30704.0_$SystemArch__8wekyb3d8bbwe.Appx"
+Add-AppxPackage -Path "$CurrentDir\Microsoft.VCLibs.140.00_14.0.30704.0_$SystemArch`__8wekyb3d8bbwe.Appx"
 Pause
