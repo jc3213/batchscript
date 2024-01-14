5918fc719f18ff76d922d0bb8e4c00ac49516cdd
jc3213 <jc3213@users.noreply.github.com>
2024-01-14 09:45:24 +0800

    Update foobar2000_portable.cmd

--- 7-zip/foobar2000_portable.cmd
+++ 7-zip/foobar2000_portable.cmd
@@ -2,8 +2,8 @@
 pushd %~dp0
 for /f "tokens=2*" %%a in ('reg query "HKLM\Software\7-Zip" /v "Path"') do (set zip=%%b7z.exe)
 for /f "tokens=1-4 delims= " %%a in ('curl https://www.foobar2000.org/download --silent ^| findstr /r /c:"/getfile/foobar2000*"') do (set ver=%%c)
-if %Processor_Architecture% equ AMD64 set arc=-x64
-if %Processor_Architecture% equ ARM64 set arc=-arm64ec
+if %processor_architecture% equ AMD64 set arc=-x64
+if %processor_architecture% equ ARM64 set arc=-arm64ec
 set app=foobar2000%arc%_%ver%
 set exe=%app%.exe
 set url=https://www.foobar2000.org/files/%exe%
