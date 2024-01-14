a6ce534be5b6f5671309d30841eec17503d69264
jc3213 <jc3213@users.noreply.github.com>
2024-01-14 10:49:10 +0800

    Update foobar2000_portable.cmd

--- 7-zip/foobar2000_portable.cmd
+++ 7-zip/foobar2000_portable.cmd
@@ -7,11 +7,11 @@ if %processor_architecture% equ ARM64 set arc=-arm64ec
 set app=foobar2000%arc%_%ver%
 set exe=%app%.exe
 set url=https://www.foobar2000.org/files/%exe%
-echo Downloading: "%exe%"
+echo Downloading : "%exe%"
 curl %url% --location --output %exe% >nul 2>&1
 "%zip%" x %exe% -y -x!$PLUGINSDIR -x!$R0 -x!uninstall.exe -o"%app%" >nul 2>&1
 type nul > %app%\portable_mode_enabled
 del /s /q %exe% >nul 2>&1
-echo Foobar2000 Portable: "%app%"
+echo Foobar2000  : "%app%"
 start "" "%app%"
 timeout /t 5
