d703422cc8a39cb71689a4793a9ea238deed43bf
jc3213 <jc3213@users.noreply.github.com>
2024-01-14 09:00:39 +0800

    Update Windows Power Plan.cmd

--- Microsoft/Windows Power Plan.cmd
+++ Microsoft/Windows Power Plan.cmd
@@ -10,13 +10,13 @@ echo 4. Manage Processor Minimum P-state
 echo 5. Manage Heterogeneous Thread Policy
 if exist "%~1" echo +. Return to Main Menu
 echo ==================================================================
-set /p poweract=^> 
-if [%poweract%] equ [1] goto :powermenu1
-if [%poweract%] equ [2] goto :powermenu2
-if [%poweract%] equ [3] goto :powermenu3
-if [%poweract%] equ [4] goto :powermenu4
-if [%poweract%] equ [5] goto :powermenu5
-if [%poweract%] equ [+] goto :backmain
+set /p sec=^> 
+if [%sec%] equ [1] goto :powermenu1
+if [%sec%] equ [2] goto :powermenu2
+if [%sec%] equ [3] goto :powermenu3
+if [%sec%] equ [4] goto :powermenu4
+if [%sec%] equ [5] goto :powermenu5
+if [%sec%] equ [+] goto :manageback
 goto :powermain
 :powermenu1
 cls
@@ -26,10 +26,10 @@ echo 0. Disable
 echo 1. Enable
 echo +. Return to Upper Menu
 echo ==================================================================
-set /p powersub=^> 
-if [%powersub%] equ [0] call :powerm1app off
-if [%powersub%] equ [1] call :powerm1app on
-if [%powersub%] equ [+] goto :powerback
+set /p sub=^> 
+if [%sub%] equ [0] call :powerm1app off
+if [%sub%] equ [1] call :powerm1app on
+if [%sub%] equ [+] goto :powerback
 goto :powermenu1
 :powerm1app
 powercfg /hibernate %1
@@ -42,10 +42,10 @@ echo 0. Never
 echo 1. Default (20 minutes)
 echo +. Return to Upper Menu
 echo ==================================================================
-set /p powersub=^> 
-if [%powersub%] equ [0] call :powerm2app 0
-if [%powersub%] equ [1] call :powerm2app 20
-if [%powersub%] equ [+] goto :powerback
+set /p sub=^> 
+if [%sub%] equ [0] call :powerm2app 0
+if [%sub%] equ [1] call :powerm2app 20
+if [%sub%] equ [+] goto :powerback
 goto :powermenu2
 :powerm2app
 powercfg /change disk-timeout-ac %1
@@ -58,10 +58,10 @@ echo ==================================================================
 echo Minimum: 50
 echo Maximum: 100 (Default)
 echo ==================================================================
-set /p powersub=^> 
-echo %powersub%| findstr /r /c:"^[5-9][0-9]$" /c:"^100$" >nul
-if %errorlevel% equ 1 set powersub=100
-call :powerm34app MAX %powersub%
+set /p sub=^> 
+echo %sub%| findstr /r /c:"^[5-9][0-9]$" /c:"^100$" >nul
+if %errorlevel% equ 1 set sub=100
+call :powerm34app MAX %sub%
 :powermenu4
 cls
 title Processor Minimum P-state - Power Plan
@@ -69,10 +69,10 @@ echo ==================================================================
 echo Minimum: 0 (Default)
 echo Maximum: 100
 echo ==================================================================
-set /p powersub=^> 
-echo %powersub%| findstr /r /c:"^[0-9]$" /c:"^[1-9][0-9]$" /c:"^100$" >nul
-if %errorlevel% equ 1 set powersub=0
-call :powerm34app MIN %powersub%
+set /p sub=^> 
+echo %sub%| findstr /r /c:"^[0-9]$" /c:"^[1-9][0-9]$" /c:"^100$" >nul
+if %errorlevel% equ 1 set sub=0
+call :powerm34app MIN %sub%
 :powermenu5
 cls
 title Heterogeneous Thread Policy - Power Plan
@@ -81,10 +81,10 @@ echo 0. Default (Automatic)
 echo 1. Prefer performant processors
 echo +. Return to Upper Menu
 echo ==================================================================
-set /p powersub=^> 
-if [%powersub%] equ [0] call :powerm5app 5
-if [%powersub%] equ [1] call :powerm5app 2
-if [%powersub%] equ [+] goto :powerback
+set /p sub=^> 
+if [%sub%] equ [0] call :powerm5app 5
+if [%sub%] equ [1] call :powerm5app 2
+if [%sub%] equ [+] goto :powerback
 goto :powermenu5
 :powerm5app
 powercfg /setacvalueindex scheme_current sub_processor SCHEDPOLICY %1
@@ -96,10 +96,9 @@ powercfg /setacvalueindex scheme_current sub_processor PROCTHROTTLE%1 %2
 powercfg /setactive scheme_current
 goto :powerback
 :powerback
-set poweract=
-set powersub=
-timeout /t 5
+set sec=
+set sub=
 goto :powermain
-:backmain
+:manageback
 if exist "%~1" call "%~1"
 goto :powermain
