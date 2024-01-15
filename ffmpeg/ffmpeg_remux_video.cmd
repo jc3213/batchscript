4f1c53c68a67ee4600e6c96f65ce7437e5b667f8
jc3213 <jc3213@users.noreply.github.com>
2024-01-15 10:06:24 +0800

    Update ffmpeg_remux_video.cmd

--- ffmpeg/ffmpeg_remux_video.cmd
+++ ffmpeg/ffmpeg_remux_video.cmd
@@ -1,25 +1,24 @@
 @echo off
 pushd %~dp0bin
 for %%a in (%*) do (call :codec %%a)
-if not defined video goto :exit
-if not defined audio goto :exit
-ffmpeg.exe -fflags +genpts %video% %audio% %image% -c:v copy -c:a copy %thumb% %output%
+ffmpeg.exe %file% %map% -c:v copy -c:a copy %output%
 :exit
 timeout /t 5
 exit
 :codec
-if %~x1 equ .jpg call :thumb %1 mjpg
-if %~x1 equ .png call :thumb %1 png
-for /f "tokens=3,4 delims= " %%a in ('ffmpeg.exe -i %1 2^>^&1 ^| findstr /i "video audio"') do (
-    if "%%a" equ "Audio:" (
-        set audio=-i %1
-    ) else if "%%a" equ "Video:" (
-        set video=-i %1
-        set output="%~dp1remuxed_%~n1.mkv"
-    )
-)
+for /f "tokens=3,4 delims= " %%a in ('ffmpeg.exe -i %1 2^>^&1 ^| findstr /i "video audio"') do (call :map "%~1" %%a)
 exit /b
-:thumb
-set image=-i %1 -map 0 -map 1 -map 2
-set thumb=-c:v:2 %2 -disposition:2 attached_pic
+:map
+echo %1 %2
+if "%~2" equ "Video:" goto :video
+if "%~2" equ "Audio:" goto :audio
+exit /b
+:video
+set file=-i %1 %file%
+set map=-map 0:v %map%
+set output="%~dpn1.mkv"
+exit /b
+:audio
+set file=%file% -i %1
+set map=%map% -map 1:a
 exit /b
