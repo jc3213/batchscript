a1237aeb509f4022cbc74c8d678cc0fa64f283c3
jc3213 <jc3213@users.noreply.github.com>
2024-01-22 16:12:16 +0800

    Update Refresh IconCache.cmd

--- Windows/Explorer/Refresh IconCache.cmd
+++ Windows/Explorer/Refresh IconCache.cmd
@@ -1,11 +1,16 @@
 @echo off
+net session >nul 2>nul && goto :admin
+mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" %*","%~dp0","runas",1)(window.close)
+exit
+:admin
 taskkill /f /im "explorer.exe"
-cd /d %LocalAppData%\Microsoft\Windows\Explorer
-for %%a in (iconcache_*.db thumbcache_*.db) do (
-    takeown /f %%a
-    icacls %%a /grant Administrators:F
-    attrib -h -s -r %%a
-    del /f /q /a %%a
-)
+cd /d %localappdata%\Microsoft\Windows\Explorer
+for %%a in (*.*) do (call :refresh "%%~a")
 start "" "explorer.exe"
 timeout /t 5
+exit
+:refresh
+takeown /f %1
+icacls %1 /grant Administrators:F
+attrib -h -s -r %1
+del /f /q /a %1
