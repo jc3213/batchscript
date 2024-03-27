3f79a1bb7a876cdbaf169f88d84045af23043699
jc3213 <jc3213@users.noreply.github.com>
2024-03-27 11:08:58 +0800

    Update Manage Microsoft Windows.cmd

--- Microsoft/Manage Microsoft Windows.cmd
+++ Microsoft/Manage Microsoft Windows.cmd
@@ -673,15 +673,10 @@ for /f "delims=" %%a in ('powershell -Command "Add-Type -AssemblyName System.win
 exit /b
 :symbolink
 if not exist "%~1" goto :symbomake
-for /f "tokens=3,4" %%a in ('fsutil reparsepoint query "%~1" ^| findstr /c:"Symbolic Link"') do (call :symbotest %1 %2 "%%a %%b")
-:symbotest
-if "%~3" equ "Symbolic Link" goto :symbotrue
-xcopy /e /i /h %1 %2
+rd %1 2>nul && goto :symbomake
+xcopy %1 %2 /e /i /h
 :symboforce
-rd /s /q %1
-goto :symbomake
-:symbotrue
-rd %1
+rd %1 /s /q
 :symbomake
 mklink /d %1 %2
 exit /b
