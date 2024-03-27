d2ac1a7428d148d7088ce481e713e5b3808448cf
jc3213 <jc3213@users.noreply.github.com>
2024-03-27 11:07:22 +0800

    Update Windows Accessibility.cmd

--- Microsoft/Windows Accessibility.cmd
+++ Microsoft/Windows Accessibility.cmd
@@ -127,17 +127,12 @@ del "%amdcpu%" "%intlcpu%" /f /q
 goto :miscback
 :symbolink
 if not exist "%~1" goto :symbomake
-for /f "tokens=3,4" %%a in ('fsutil reparsepoint query "%~1" ^| findstr /c:"Symbolic Link"') do (call :symbotest %1 %2 "%%a %%b")
-:symbotest
-if "%~3" equ "Symbolic Link" goto :symbotrue
-xcopy /e /i /h %1 %2
+rd %1 2>nul && goto :symbomake
+xcopy %1 %2 /e /i /h
 :symboforce
-rd /s /q %1
-goto :symbomake
-:symbotrue
-rd %1
+rd %1 /s /q
 :symbomake
-mklink /d %1 %2
+mklink /d %1 "%drive%\Home\%~n1"
 exit /b
 :miscback
 set miscact=
