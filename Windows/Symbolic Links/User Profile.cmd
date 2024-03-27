4e044322e03bde1bb244f330e82020db124deb25
jc3213 <jc3213@users.noreply.github.com>
2024-03-27 11:03:45 +0800

    Update User Profile.cmd

--- Windows/Symbolic Links/User Profile.cmd
+++ Windows/Symbolic Links/User Profile.cmd
@@ -18,13 +18,9 @@ timeout /t 5
 goto :eof
 :symbolink
 if not exist "%~1" goto :symbomake
-for /f "tokens=3,4" %%a in ('fsutil reparsepoint query "%~1" ^| findstr /c:"Symbolic Link"') do (call :symbotest %1 %2 "%%a %%b")
-:symbotest
-if "%~3" equ "Symbolic Link" goto :symbotrue
-xcopy /e /i /h %1 %2
-rd /s /q %1
+rd %1 2>nul && goto :symbomake
+xcopy %1 %2 /e /i /h
+rd %1 /s /q
 goto :symbomake
-:symbotrue
-rd %1
 :symbomake
 mklink /d %1 "%drive%\Home\%~n1"
