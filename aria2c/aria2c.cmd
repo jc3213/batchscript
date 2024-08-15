9761f0ca66cca7052bf658957efef500da09ae48
jc3213 <jc3213@users.noreply.github.com>
2024-08-15 11:37:53 +0800

    Update aria2c.cmd

--- aria2c/aria2c.cmd
+++ aria2c/aria2c.cmd
@@ -1,12 +1,16 @@
 @echo off
+setlocal enabledelayedexpansion
 pushd %~dp0
+for /f "delims=" %%a in ('dir /s /b aria2c.exe') do (set aria2c=%%a)
+set aria2c=!aria2c:%~dp0=!
 if not exist aria2c.session type nul > aria2c.session
 if [%1] equ [/s] goto :nowindow
 if [%1] equ [/r] goto :register
 if [%1] equ [/u] goto :Unregister
-bin\aria2c.exe --conf=aria2c.conf
+"%aria2c%" --conf=aria2c.conf
+exit
 :register
-reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /t "REG_SZ" /d "mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"bin\aria2c.exe\",\"--conf=aria2c.conf\",\"%~dp0",\"\",0)(window.close)" /f
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Run" /v "aria2c" /t "REG_SZ" /d "mshta vbscript:CreateObject(\"Shell.Application\").ShellExecute(\"%aria2c%\",\"--conf=aria2c.conf\",\"%~dp0",\"\",0)(window.close)" /f
 :nowindow
 mshta vbscript:CreateObject("Shell.Application").ShellExecute("bin\aria2c.exe","--conf=aria2c.conf","%~dp0","",0)(window.close)
 exit
