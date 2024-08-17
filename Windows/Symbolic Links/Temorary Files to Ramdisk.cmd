241f09efdcb3d2a5b85e61e4580e394fe90c6868
jc3213 <jc3213@users.noreply.github.com>
2024-08-17 23:57:52 +0800

    Update Temorary Files to Ramdisk.cmd

--- Windows/Symbolic Links/Temorary Files to Ramdisk.cmd
+++ Windows/Symbolic Links/Temorary Files to Ramdisk.cmd
@@ -6,5 +6,13 @@ call :symbolink "%systemroot%\Temp"
 timeout /t 5
 goto :eof
 :symbolink
-rd %1 2>nul || rd %1 /s /q
-mklink /d %1 %ramdisk%
+if not exist "%~1" goto :symbomake
+for /f "tokens=3,4" %%a in ('fsutil reparsepoint query "%~1" ^| findstr /c:"Symbolic Link"') do (call :symbotest %1 %2 "%%a %%b")
+:symbotest
+if "%~3" equ "Symbolic Link" goto :symbotrue
+rd /s /q %1
+goto :symbomake
+:symbotrue
+rd %1
+:symbomake
+mklink /d %1 "%ramdisk%"
