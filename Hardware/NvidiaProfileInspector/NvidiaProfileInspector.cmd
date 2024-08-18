0727c20d8a0168f962368b6d2d5603b2ea06b483
jc3213 <jc3213@users.noreply.github.com>
2024-08-18 11:04:19 +0800

    Support proxy servre when fallback

--- Hardware/NvidiaProfileInspector/NvidiaProfileInspector.cmd
+++ Hardware/NvidiaProfileInspector/NvidiaProfileInspector.cmd
@@ -1,12 +1,14 @@
 @echo off
 for /f %%a in ('wmic logicaldisk where "VolumeName='RAMDISK'" get Caption ^| find ":"') do (set ramdisk=%%a)
 set package=NvidiaProfileInspector.zip
-set app=%ramdisk%\%package%
-set folder=%app:~0,-4%
+set app=%ramdisk%\Temp\%package%
 curl "https://github.com/Orbmu2k/nvidiaProfileInspector/releases/latest/download/%package%" --location --output "%app%"
 if %errorlevel% equ 0 goto :extract
-curl "https://github.com/Orbmu2k/nvidiaProfileInspector/releases/latest/download/%package%" --location --output "%app%" --proxy "127.0.0.1:7890"
+:proxy
+set /p proxy=Proxy Server: 
+for /f "tokens=1 delims=:" %%a in ("%proxy%") do (set test=%%a)
+ping %test% >nul 2>nul || goto :proxy
+curl "https://github.com/Orbmu2k/nvidiaProfileInspector/releases/latest/download/%package%" --location --output "%app%" --proxy "%proxy%"
 :extract
-md "%folder%" >nul 2>nul
-tar -xf %package% -C "%folder%"
+tar -xf "%app%" -C "%~dp0\"
 timeout /t 5
