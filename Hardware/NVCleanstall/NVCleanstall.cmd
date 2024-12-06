7da8e93b5dc8d63c29479d693903444bcfe564bf
jc3213 <jc3213@users.noreply.github.com>
2024-12-06 13:15:13 +0800

    Update NVCleanstall.cmd

--- Hardware/NVCleanstall/NVCleanstall.cmd
+++ Hardware/NVCleanstall/NVCleanstall.cmd
@@ -1,10 +1,12 @@
 @echo off
-set url=https://www.techpowerup.com/download/techpowerup-nvcleanstall/
-set app=NVCleanstall.exe
-for /f "tokens=7 delims== " %%i in ('curl "%url%" ^| findstr "name=\"id\""') do ( call :app %%i)
-:app
+set app=techpowerup-nvcleanstall
+set exe=NVCleanstall.exe
+set url=https://www.techpowerup.com/download/%app%/
+for /f "tokens=7 delims== " %%i in ('curl "%url%" ^| findstr "name=\"id\""') do ( call :download %%i)
+:download
 for /f %%a in ('wmic logicaldisk where "VolumeName='RAMDISK'" get Caption ^| find ":"') do (set ramdisk=%%a)
-curl "%url%" --data-raw "id=%~1&server_id=16" --location --output "%ramdisk%\%app%"
-xcopy "%app%" "%~dp0" /y 2>nul
+if not exist "%ramdisk%" (set ramdisk=%temp%)
+curl "%url%" --data-raw "id=%~1&server_id=16" --location --output "%ramdisk%\%exe%"
+xcopy "%exe%" "%~dp0" /y 2>nul
 timeout /t 5
 exit
