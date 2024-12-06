eda59d9b6d5a2a7cdd3cbb99518c9ae21321794e
jc3213 <jc3213@users.noreply.github.com>
2024-12-06 13:15:33 +0800

    Update GPU-Z.cmd

--- Hardware/GPU-Z/GPU-Z.cmd
+++ Hardware/GPU-Z/GPU-Z.cmd
@@ -2,9 +2,10 @@
 set app=techpowerup-gpu-z
 set exe=GPU-Z.exe
 set url=https://www.techpowerup.com/download/%app%/
-for /f "tokens=7 delims== " %%i in ('curl "%url%" ^| findstr "name=\"id\""') do ( call :download %%i)
+for /f "tokens=7 delims== " %%i in ('curl "%url%" ^| findstr "name=\"id\""') do (call :download %%i)
 :download
 for /f %%a in ('wmic logicaldisk where "VolumeName='RAMDISK'" get Caption ^| find ":"') do (set ramdisk=%%a)
+if not exist "%ramdisk%" (set ramdisk=%temp%)
 curl "%url%" --data-raw "id=%~1&server_id=16" --location --output "%ramdisk%\%exe%"
 xcopy "%exe%" "%~dp0" /y 2>nul
 timeout /t 5
