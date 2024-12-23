564df0e1fec247a4089803b75e68126c031899e5
jc3213 <jc3213@users.noreply.github.com>
2024-12-23 14:34:40 +0800

    Update GPU-Z.cmd

--- Hardware/GPU-Z/GPU-Z.cmd
+++ Hardware/GPU-Z/GPU-Z.cmd
@@ -7,6 +7,6 @@ for /f "tokens=7 delims== " %%i in ('curl "%url%" ^| findstr "name=\"id\""') do 
 for /f %%a in ('wmic logicaldisk where "VolumeName='RAMDISK'" get Caption ^| find ":"') do (set ramdisk=%%a)
 if not exist "%ramdisk%" (set ramdisk=%temp%)
 curl "%url%" --data-raw "id=%~1&server_id=16" --location --output "%ramdisk%\%exe%"
-xcopy "%exe%" "%~dp0" /y 2>nul
+xcopy "%ramdisk%\%exe%" "%~dp0" /y 2>nul
 timeout /t 5
 exit
