e46c64149761a01a70daa72b7454c525f959824b
jc3213 <jc3213@users.noreply.github.com>
2025-02-14 23:20:01 +0800

    Update magick_utilities.cmd

--- magick/magick_utilities.cmd
+++ magick/magick_utilities.cmd
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 title ImageMagick Utilities
 :menu
 cls
@@ -170,5 +171,37 @@ echo Processing: %~dpnx1
 echo Output file: "%output%"
 exit /b
 :main
+set start=%time%
 for %%a in (%*) do (call :imagick "%%~a")
-timeout /t 5
+set finish=%time%
+set /a hour=%finish:~0,2%-%start:~0,2%
+set /a minute=%finish:~3,2%-%start:~3,2%
+set /a second=%finish:~6,2%-%start:~6,2%
+set /a millisecond=%finish:~9,2%-%start:~9,2%
+if %millisecond% lss 0 (
+    set /a millisecond+=100
+    set /a second-=1
+)
+if %second% lss 0 (
+    set /a second+=60
+    set /a minute-=1
+)
+if %minute% lss 0 (
+    set /a minute+=60
+    set /a hour-=1
+)
+if %hour% lss 0 (
+    set /a hour+=24
+)
+if %hour% neq 0 (
+    set dur=%hour%:%minute%:%second%.%millisecond%
+) else if %minute% neq 0 (
+    set dur=%minute%:%second%.%millisecond%
+) else (
+    set dur=%second%.%millisecond%
+)
+echo.
+echo.
+echo Elapsed   : %dur%
+endlocal
+timeout /t 10
