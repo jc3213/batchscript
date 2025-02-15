74b2b3ad693313a4da2e7cf399612da52c8b0ccf
jc3213 <jc3213@users.noreply.github.com>
2025-02-15 23:28:04 +0800

    Update magick_utilities.cmd

--- magick/magick_utilities.cmd
+++ magick/magick_utilities.cmd
@@ -3,13 +3,13 @@ setlocal enabledelayedexpansion
 title ImageMagick Utilities
 :menu
 cls
-echo ============================================================
+echo ===================================================================
 echo 1. Crop with area
 echo 2. Cut off border
 echo 3. Convert format
 echo 4. Resize images
 echo 5. Darken images
-echo ============================================================
+echo ===================================================================
 set /p op=^> 
 if [%op%] equ [1] goto :crop
 if [%op%] equ [2] goto :shave
@@ -48,7 +48,7 @@ goto :main
 :area
 echo.
 echo.
-echo ============================================================
+echo ===================================================================
 echo https://imagemagick.org/script/command-line-processing.php#geometry
 echo Sample: 300x (width)
 echo Cut 300px from both left and right of the image
@@ -58,20 +58,20 @@ echo Sample: 300x100+20+30 (width x height + left + top)
 echo Crop image area start from: left 20px to 320px, top 30px to 130px
 echo Sample: 200x+50 (width + left)
 echo Crop image area start from: left 50px to 250px, height 100%%
-echo ============================================================
+echo ===================================================================
 set /p area=^> 
 if not defined area goto :area
 exit /b
 :outxxx
 echo.
 echo.
-echo ============================================================
+echo ===================================================================
 echo 1. jpg
 echo 2. png
 echo 3. avif
 echo 4. webp
 echo *. Keep Original Format [Default]
-echo ============================================================
+echo ===================================================================
 set /p fm=^> 
 if [%fm%] equ [1] set format=.jpg
 if [%fm%] equ [2] set format=.png
@@ -81,12 +81,12 @@ exit /b
 :output
 echo.
 echo.
-echo ============================================================
+echo ===================================================================
 echo 1. jpg
 echo 2. png [Default]
 echo 3. avif
 echo 4. webp
-echo ============================================================
+echo ===================================================================
 set /p fm=^> 
 if [%fm%] equ [1] set format=.jpg
 if [%fm%] equ [3] set format=.avif
@@ -96,27 +96,27 @@ exit /b
 :quality
 echo.
 echo.
-echo ============================================================
+echo ===================================================================
 echo Set image quality: 1-100
 echo Default: 90
-echo ============================================================
+echo ===================================================================
 set /p qu=^> 
 echo %qu%| findstr /r "^[1-9]$ ^[1-9][0-9]$ ^100$" >nul || set qu=90
 exit /b
 :level
 echo.
 echo.
-echo ============================================================
+echo ===================================================================
 echo Set minimum color level: 0-100
 echo Default: 30
-echo ============================================================
+echo ===================================================================
 set /p lv=^> 
 echo %lv%| findstr /r "^[1-9]$ ^[1-9][0-9]$ ^100$" >nul || set lv=30
 exit /b
 :size
 echo.
 echo.
-echo ============================================================
+echo ===================================================================
 echo Sample: 300x100 (width x height)
 echo Resize image to 300px width and 100px height
 echo Sample: 500x (width)
@@ -124,12 +124,12 @@ echo Sample: x400 (height)
 echo Resize image and keep aspect ratio
 echo Sample: 50%%%%
 echo Resize image to 50%% of its size
-echo ============================================================
+echo ===================================================================
 set /p size=^> 
 if not defined size goto :size
 echo.
 echo.
-echo ============================================================
+echo ===================================================================
 echo 1. Lanczos Filter [Default]
 echo 2. Lanczos2 Filter
 echo 3. Hermite Filter
@@ -137,7 +137,7 @@ echo 4. Mitchell Filter *Recommended for enlarging*
 echo 5. Hamming Filter
 echo 6. Catrom Filter
 echo 7. Gaussian Filter
-echo ============================================================
+echo ===================================================================
 set /p ft=^> 
 if [%ft%] equ [2] set filter=Lanczos2
 if [%ft%] equ [3] set filter=Hermite
@@ -147,28 +147,6 @@ if [%ft%] equ [6] set filter=Catrom
 if [%ft%] equ [7] set filter=Gaussian
 if defined ft set filter=Lanczos
 set params=%params% -filter %filter%
-exit /b
-:imagick
-cd /d %1 2>nul
-if %errorlevel% equ 0 goto :folder
-if not defined format set format=%~x1
-set output=%~dpn1 %name%%format%
-call :appx %1
-exit /b
-:folder
-set folder=%~1 %name%
-md "%folder%" 2>nul
-for %%a in (*) do (call :files "%%~a")
-exit /b
-:files
-if not defined format set format=%~x1
-set output=%folder%\%~n1%format%
-:appx
-echo.
-echo.
-echo Processing: %~dpnx1
-"%~dp0bin\magick.exe" "%~1" %params% "%output%"
-echo Output file: "%output%"
 exit /b
 :main
 set start=%time%
@@ -205,3 +183,26 @@ echo.
 echo Elapsed   : %dur%
 endlocal
 pause
+exit
+:imagick
+cd /d %1 2>nul
+if %errorlevel% equ 0 goto :folder
+if not defined format set format=%~x1
+set output=%~dpn1 %name%%format%
+goto :result
+exit /b
+:folder
+set folder=%~1 %name%
+md "%folder%" 2>nul
+for %%a in (*) do (call :files "%%~a")
+exit /b
+:files
+if not defined format set format=%~x1
+set output=%folder%\%~n1%format%
+:result
+echo.
+echo.
+echo Processing: %~dpnx1
+"%~dp0bin\magick.exe" "%~1" %params% "%output%"
+echo Output file: "%output%"
+exit /b
