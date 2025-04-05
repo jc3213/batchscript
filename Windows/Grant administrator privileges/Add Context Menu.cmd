4bd33af6bfed1661f54e47ec1073d52360bae848
jc3213 <jc3213@users.noreply.github.com>
2025-04-05 21:11:50 +0800

    Add i18n

--- Windows/Grant administrator privileges/Add Context Menu.cmd
+++ Windows/Grant administrator privileges/Add Context Menu.cmd
@@ -1,21 +1,29 @@
 @echo off
-set File=HKCR\*\shell\runas
-set Folder=HKCR\Directory\shell\runas
-set Menu=Grant administrator privileges
-set Icon=C:\Windows\system32\imageres.dll,-78
-set Admin=%%SystemRoot%%\system32\cmd.exe /c takeown /f \"%%1\" ^&^& icacls \"%%1\" /grant Administrators:F
-set Admin@=%%SystemRoot%%\system32\cmd.exe /c takeown /f \"%%1\" /r /d y ^&^& icacls \"%%1\" /grant Administrators:F /t
-::File
-reg add "%File%" /ve /t "REG_SZ" /d "%Menu%" /f
-reg add "%File%" /v "Icon" /t "REG_SZ" /d "%Icon%" /f
-reg add "%File%" /v "NoWorkingDirectory"  /t "REG_SZ" /d "" /f
-reg add "%File%\command" /ve /t "REG_EXPAND_SZ" /d "%Admin%" /f
-reg add "%File%\command" /v "IsolatedCommand" /t "REG_EXPAND_SZ" /d "%Admin%" /f
-::Folder
-reg add "%Folder%" /ve /t "REG_SZ" /d "%Menu%" /f
-reg add "%Folder%" /v "Icon" /t "REG_SZ" /d "%Icon%" /f
-reg add "%Folder%" /v "NoWorkingDirectory" /t "REG_SZ" /d "" /f
-reg add "%Folder%\command" /ve /t "REG_EXPAND_SZ" /d "%Admin@%" /f
-reg add "%Folder%\command" /v "IsolatedCommand" /t "REG_EXPAND_SZ" /d "%Admin@%" /f
-::End
+net session >nul 2>nul && goto :runasadmin
+mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" %*","","runas",1)(window.close)
+exit
+:runasadmin
+setlocal
+for /f "tokens=2 delims==" %%a in ('wmic os get Locale /value') do (set locale=%%a)
+if "%locale%" equ "0804" call :main "授予管理员权限"
+if "%locale%" equ "0404" call :main "授予管理員權限"
+if "%locale%" equ "0411" call :main "管理者権限を付与します"
+call :main "Grant administrator privileges"
+:main
+set icon=C:\Windows\system32\imageres.dll,-78
+set admin=%%SystemRoot%%\system32\cmd.exe /c takeown /f \"%%1\" ^&^& icacls \"%%1\" /grant Administrators:F
+set admin@=%%SystemRoot%%\system32\cmd.exe /c takeown /f \"%%1\" /r /d y ^&^& icacls \"%%1\" /grant Administrators:F /t
+:file
+reg add "HKCR\*\shell\runas" /ve /t "REG_SZ" /d %1 /f
+reg add "HKCR\*\shell\runas" /v "Icon" /t "REG_SZ" /d "%icon%" /f
+reg add "HKCR\*\shell\runas" /v "NoWorkingDirectory"  /t "REG_SZ" /d "" /f
+reg add "HKCR\*\shell\runas\command" /ve /t "REG_EXPAND_SZ" /d "%admin%" /f
+reg add "HKCR\*\shell\runas\command" /v "IsolatedCommand" /t "REG_EXPAND_SZ" /d "%admin%" /f
+:folder
+reg add "HKCR\Directory\shell\runas" /ve /t "REG_SZ" /d %menu% /f
+reg add "HKCR\Directory\shell\runas" /v "Icon" /t "REG_SZ" /d "%icon%" /f
+reg add "HKCR\Directory\shell\runas" /v "NoWorkingDirectory" /t "REG_SZ" /d "" /f
+reg add "HKCR\Directory\shell\runas\command" /ve /t "REG_EXPAND_SZ" /d "%admin@%" /f
+reg add "HKCR\Directory\shell\runas\command" /v "IsolatedCommand" /t "REG_EXPAND_SZ" /d "%admin@%" /f
 timeout /t 5
+exit
--- Windows/Grant administrator privileges/Remove Context Menu.cmd
+++ Windows/Grant administrator privileges/Remove Context Menu.cmd
@@ -1,4 +1,8 @@
 @echo off
+net session >nul 2>nul && goto :runasadmin
+mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" %*","","runas",1)(window.close)
+exit
+:runasadmin
 reg delete "HKCR\*\shell\runas" /f
 reg delete "HKCR\Directory\shell\runas" /f
 timeout /t 5
