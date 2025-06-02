2a7d95ddc2ab0782aae7bee19cee5d80731aad9d
jc3213 <jc3213@users.noreply.github.com>
2025-06-02 16:30:04 +0800

    Update upscale_ncnn_utilities.cmd

--- upscale/upscale_ncnn_utilities.cmd
+++ upscale/upscale_ncnn_utilities.cmd
@@ -148,32 +148,21 @@ if [%tta%] equ [1] echo TTA Mode   :   Enabled
 echo ===================================================================
 for %%a in (%*) do (call :upscale "%%~a")
 set finish=%time%
-set /a hour=%finish:~0,2%-%start:~0,2%
-set /a minute=%finish:~3,2%-%start:~3,2%
-set /a second=%finish:~6,2%-%start:~6,2%
-set /a millisecond=%finish:~9,2%-%start:~9,2%
-if %millisecond% lss 0 (
-    set /a millisecond+=100
-    set /a second-=1
-)
-if %second% lss 0 (
-    set /a second+=60
-    set /a minute-=1
-)
-if %minute% lss 0 (
-    set /a minute+=60
-    set /a hour-=1
-)
-if %hour% lss 0 (
-    set /a hour+=24
-)
+set /a stsec=%start:~0,2%*360000+%start:~3,2%*6000+%start:~6,2%*100+%start:~9,2%
+set /a finsec=%finish:~0,2%*360000+%finish:~3,2%*6000+%finish:~6,2%*100+%finish:~9,2%
+if %finsec% lss %stsec% set /a finsec+=8640000
+set /a elapsed=%finsec%-%stsec%
+set /a hour=elapsed/360000
+set /a minute=(elapsed%%360000)/6000
+set /a second=(elapsed%%6000)/100
+set /a millsec=elapsed%%100
 if %hour% lss 10 set hour=0%hour%
 if %minute% lss 10 set minute=0%minute%
 if %second% lss 10 set second=0%second%
-if %millisecond% lss 10 set millisecond=0%millisecond%
+if %millsec% lss 10 set millsec=0%millsec%
 echo.
 echo.
-echo Elapsed   : %hour%:%minute%:%second%.%millisecond%
+echo Elapsed   : %hour%:%minute%:%second%.%millsec%
 endlocal
 pause
 exit
