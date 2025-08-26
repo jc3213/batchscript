ee400827dcafa534ca2d66df93a82ceed490d5fc
jc3213 <jc3213@users.noreply.github.com>
2025-08-27 00:12:47 +0800

    Update magick_utilities.cmd

--- magick/magick_utilities.cmd
+++ magick/magick_utilities.cmd
@@ -1,5 +1,5 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal
 title ImageMagick Utilities
 :menu
 cls
@@ -168,6 +168,6 @@ echo.
 echo.
 echo Processing : "%~dpnx1"
 if not defined format set format=%~x1
-%~dp0magick\magick.exe" "%~1" %params% "%folder%%~n1%output%%format%" >nul 2>nul
+"%~dp0magick\magick.exe" "%~1" %params% "%folder%%~n1%output%%format%" >nul 2>nul
 echo Result     : "%folder%%~n1%output%%format%"
 exit /b
