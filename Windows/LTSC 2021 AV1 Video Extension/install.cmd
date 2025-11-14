3d4acd2e90f0a1eb78ea90fcc62adaad326fdb1e
jc3213 <jc3213@users.noreply.github.com>
2025-11-14 19:11:15 +0800

    Delete Windows/LTSC 2021 AV1 Video Extension directory

--- Windows/LTSC 2021 AV1 Video Extension/install.cmd
+++
@@ -1,5 +0,0 @@
-@echo off
-pushd %~dp0
-if %Processor_Architecture% equ AMD64 (set Arch=x64) else (set Arch=x86)
-%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -Command "Add-AppxPackage -Path 'Microsoft.AV1VideoExtension_1.1.52851.0_%Arch%__8wekyb3d8bbwe.Appx'"
-timeout /t 5
--- Windows/LTSC 2021 AV1 Video Extension/install.ps1
+++
@@ -1,7 +0,0 @@
-if ([Environment]::Is64BitOperatingSystem) {
-    $SystemArch = "x64"
-} else {
-    $SystemArch = "x86"
-}
-Add-AppxPackage -Path "$PSScriptRoot\Microsoft.AV1VideoExtension_1.1.52851.0_$SystemArch`__8wekyb3d8bbwe.Appx"
-Pause
