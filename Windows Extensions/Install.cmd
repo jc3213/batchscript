42232263a2d8f314436e6b0faddf3119236b09d6
jc3213 <jc3213@users.noreply.github.com>
2025-11-14 19:19:51 +0800

    Update install.cmd

--- Windows Extensions/Install.cmd
+++ Windows Extensions/Install.cmd
@@ -5,31 +5,31 @@ title Windows Extensions
 :main
 cls
 echo ===================================================================
-echo 1. WebP Image Extension
-echo 2. HEIF Image Extension
-echo 3. AV1 Video Extension
-echo 4. HEVC Video Extensions
-echo 5. VP9 Video Extensions
-echo 6. MPEG-2 Video Extension
-echo a. Install image extensions
-echo b. Install video extensions
-echo 0. Install all extensions
+echo 1. WebP image extension
+echo 2. HEIF image extension
+echo 3. AV1 video extension
+echo 4. HEVC video extensions
+echo 5. VP9 video extensions
+echo 6. MPEG-2 video extension
+echo a. All image extensions
+echo b. All video extensions
+echo 0. All video and image extensions
 echo ===================================================================
-set /p sel=^> 
-if "%sel%" equ "1" call :install "WebP Image Extension" "WebpImage"
-if "%sel%" equ "2" call :install "HEIF Image Extension" "HEIFImage"
-if "%sel%" equ "3" call :install "AV1 Video Extension" "AV1Video"
-if "%sel%" equ "4" call :install "HEVC Video Extensions" "HEVCVideo"
-if "%sel%" equ "5" call :install "VP9 Video Extensions" "VP9Video"
-if "%sel%" equ "6" call :install "MPEG-2 Video Extension" "MPEG2Video"
-if "%sel%" equ "a" call :install "Image Extensions" "*Image"
-if "%sel%" equ "b" call :install "Video Extensions" "*Video"
-if "%sel%" equ "0" call :install "All Extensions" "*"
+set /p app=^> 
+if "%app%" equ "1" call :install "WebP image extension" "WebpImage"
+if "%app%" equ "2" call :install "HEIF image extension" "HEIFImage"
+if "%app%" equ "3" call :install "AV1 video extension" "AV1Video"
+if "%app%" equ "4" call :install "HEVC video extensions" "HEVCVideo"
+if "%app%" equ "5" call :install "VP9 video extensions" "VP9Video"
+if "%app%" equ "6" call :install "MPEG-2 video extension" "MPEG2Video"
+if "%app%" equ "a" call :install "All image extensions" "*Image"
+if "%app%" equ "b" call :install "All video extensions" "*Video"
+if "%app%" equ "0" call :install "All video and image extensions" "*"
 goto :main
 :install
 echo.
 powershell -Command "Add-AppxPackage -Path 'Microsoft.%~2Extension*_neutral_~_8wekyb3d8bbwe.AppxBundle'"
-echo %~1 has been installed successfully!
 echo.
+set app=
 timeout /t 5
-exit /b
+goto :main
