2dac7433486ade9fb8175af69cc6a0f172fd459c
jc3213 <jc3213@users.noreply.github.com>
2025-12-06 17:05:59 +0800

    Update User Profile - Registry.cmd

--- Folder Redirect/User Profile - Registry.cmd
+++ Folder Redirect/User Profile - Registry.cmd
@@ -1,21 +1,25 @@
 @echo off
-setlocal
-:input
+net session >nul 2>nul && goto :admin
+mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" %*","","runas",1)(window.close)
+exit
+:registry
+reg add "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders" /v "%~1" /t REG_EXPAND_SZ /d "%drive%\Home\%~2" /f
+exit /b
+:admin
 echo ====================================================
 echo Pleas enter the disk label
 echo For example, D or D: or D:\
 echo ====================================================
 set /p label=^>
 set drive=%label:~0,1%:
-if not exist "%drive%" cls && goto :input
+if not exist "%drive%" cls && goto :admin
+call :registry "Desktop" "Desktop"
+call :registry "Personal" "Documents"
+call :registry "Downloads" Downloads"
+call :registry "My Music" "Music"
+call :registry "My Pictures" "Pictures"
+call :registry "My Video" "Videos"
 taskkill /f /im explorer.exe >nul 2>nul
-set UserShell=HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\User Shell Folders
-reg add "%UserShell%" /v Desktop /t REG_EXPAND_SZ /d "%drive%\Home\Desktop" /f
-reg add "%UserShell%" /v Personal /t REG_EXPAND_SZ /d "%drive%\Home\Documents" /f
-reg add "%UserShell%" /v Downloads /t REG_EXPAND_SZ /d "%drive%\Home\Downloads" /f
-reg add "%UserShell%" /v My Music /t REG_EXPAND_SZ /d "%drive%\Home\Music" /f
-reg add "%UserShell%" /v My Pictures /t REG_EXPAND_SZ /d "%drive%\Home\Pictures" /f
-reg add "%UserShell%" /v My Video /t REG_EXPAND_SZ /d "%drive%\Home\Videos" /f
 start explorer.exe
 endlocal
 timeout /t 5
