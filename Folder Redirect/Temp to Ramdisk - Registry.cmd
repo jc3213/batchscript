82b522b3fce0778c565ae24f29fb46d9392eada4
jc3213 <jc3213@users.noreply.github.com>
2025-12-06 16:23:34 +0800

    Update Temp to Ramdisk - Registry.cmd

--- Folder Redirect/Temp to Ramdisk - Registry.cmd
+++ Folder Redirect/Temp to Ramdisk - Registry.cmd
@@ -2,18 +2,17 @@
 net session >nul 2>nul && goto :admin
 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c ""%~s0"" %*","","runas",1)(window.close)
 exit
+:registry
+reg add "%~1" /v "TEMP" /t "REG_EXPAND_SZ" /d "%ramdisk%" /f
+reg add "%~1" /v "TMP" /t "REG_EXPAND_SZ" /d "%ramdisk%" /f
+exit /b
 :admin
 setlocal
 for /f %%a in ('wmic logicaldisk where "VolumeName='RAMDISK'" get Caption ^| find ":"') do (set ramdisk=%%a\Temp)
 if not exist "%ramdisk%" goto :exit
-taskkill /f /im explorer.exe >nul 2>nul
-set "UserEnv=HKCU\Environment"
-reg add "%UserEnv%" /v TEMP /t REG_EXPAND_SZ /d "%ramdisk%" /f
-reg add "%UserEnv%" /v TMP  /t REG_EXPAND_SZ /d "%ramdisk%" /f
-set "SysEnv=HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
-reg add "%SysEnv%" /v TEMP /t REG_EXPAND_SZ /d "%ramdisk%" /f
-reg add "%SysEnv%" /v TMP  /t REG_EXPAND_SZ /d "%ramdisk%" /f
-start explorer.exe
+call :registry "HKCU\Environment"
+call :registry "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment"
+taskkill /f /im explorer.exe >nul 2>nul && start explorer.exe
 :exit
 endlocal
 timeout /t 5
