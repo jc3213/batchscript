6384ae7ba6fa4024d303060c4dbd022c7abd43b3
jc3213 <jc3213@users.noreply.github.com>
2026-01-27 23:54:10 +0800

    Update and rename manga_fixer_extreme.cmd to image_fixer_extreme.cmd

--- Manga/image_fixer_extreme.cmd
+++ Manga/image_fixer_extreme.cmd
@@ -41,11 +41,15 @@ exit /b
 echo.
 echo.
 echo Fixing   : "%~dpnx1"
-"%~dp0upscaler\realcugan-ncnn-vulkan.exe" -i "%~1" -o "%folder%\temp_cugan_%~n1.png" -m models-se -s 2 -n 1 -t 32 -x >nul 2>nul
-"%~dp0upscaler\realesrgan-ncnn-vulkan.exe" -i "%~1" -o "%folder%\temp_esrgan_%~n1.png" -n realesr-animevideov3 -s 2 -t 32 -x >nul 2>nul
-"%~dp0upscaler\waifu2x-ncnn-vulkan.exe" -i "%~1" -o "%folder%\temp_waifu2x_%~n1.png" -m models-cunet -s 2 -n 1 -t 32 -x >nul 2>nul
-"%~dp0magick\magick.exe" "%folder%\temp_esrgan_%~n1.png" "%folder%\temp_cugan_%~n1.png" "%folder%\temp_waifu2x_%~n1.png" "%folder%\temp_esrgan_%~n1.png" -evaluate-sequence mean "%folder%\temp_eval_%~n1.png" >nul 2>nul
-"%~dp0magick\magick.exe" "%folder%\temp_eval_%~n1.png" -resize x1600 -quality 90 "%folder%\result_%~n1.jpg" >nul 2>nul
-del /f /s /q "%folder%\temp_cugan_%~n1.png" "%folder%\temp_esrgan_%~n1.png" "%folder%\temp_waifu2x_%~n1.png" "%folder%\temp_eval_%~n1.png" >nul 2>nul
+set cugan=%folder%\temp_cugan_%~n1.png
+set esrgan=%folder%\temp_esrgan_%~n1.png
+set waifu=%folder%\temp_waifu2x_%~n1.png
+set eval=%folder%\temp_eval_%~n1.png
+"%~dp0upscaler\realcugan-ncnn-vulkan.exe" -i "%~1" -o "%cugan%" -m models-se -s 2 -n 1 -t 32 -x
+"%~dp0upscaler\realesrgan-ncnn-vulkan.exe" -i "%~1" -o "%esrgan%" -n realesr-animevideov3 -s 2 -t 32 -x
+"%~dp0upscaler\waifu2x-ncnn-vulkan.exe" -i "%~1" -o "%waifu%" -m models-cunet -s 2 -n 1 -t 32 -x
+"%~dp0magick\magick.exe" "%waifu%" "%cugan%" "%esrgan%" "%waifu%" -evaluate-sequence mean "%eval%"
+::"%~dp0magick\magick.exe" "%eval%" -resize x2046 -quality 90 "%folder%\result_%~n1.jpg" >nul 2>nul
+::del /f /q "%waifu%" "%cugan%" "%esrgan%" >nul 2>nul
 echo Output   : "%folder%\result_%~n1.jpg"
 exit /b
