164f82762b2d3351e3ce164522e0070fb85414cd
jc3213 <jc3213@users.noreply.github.com>
2026-01-31 11:27:38 +0800

    Create download_latest_quickjs.cmd

---
+++ youtube-dl/download_latest_quickjs.cmd
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+pushd %~dp0
+for /f "usebackq tokens=2 delims=:}" %%a in (`curl -s "https://bellard.org/quickjs/binary_releases/LATEST.json"`) do (set version=%%a)
+set version=%version:"=%
+set file=quickjs-win-x86_64-%version%.zip
+curl -L -o "%file%" "https://bellard.org/quickjs/binary_releases/%file%"
+powershell -command "Expand-Archive -Force '%file%' '.'"
+timeout /t 5
